6a2a657443e2a2ef70ebdc1fff3282c4681ea7b2
Tony <awalker780@gmail.com>
2023-01-25 10:38:31 -0500

    ASCII art using my initials git commit -m ASCII art using my initials

---
+++ block_letters.py
@@ -0,0 +1,9 @@
+# ASCII art using my initials
+
+print("  A   W   W")
+print(" A A  W   W")
+print("A   A W   W")
+print("AAAAA W W W")
+print("A   A WW WW")
+print("A   A WW WW")
+print("A   A W   W")
