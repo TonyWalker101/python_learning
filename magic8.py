5b387bfb11e89cdd95ddddab35f41e71e60cfbd8
Tony <awalker780@gmail.com>
2023-01-26 08:26:46 -0500

    finalizes program

--- magic8.py
+++ magic8.py
@@ -1,10 +1,39 @@
 #magic 8 ball program that responds to user questions
+
 import random
 
-name = input("Hello! What is your name?")
+# setup
+name = input("Hello! What is your name? ")
 
-question = "Will this program work?"
+question = input("Ask me a Yes or No question: ")
 
 answer = ""
 
-# print(name)
+# answer logic
+random_number = random.randint(1,9)
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+else:
+  answer = "Error"
+
+#final output
+
+print(name," asks: ", question)
+print("Magic 8-Ball's answer: ", answer)
