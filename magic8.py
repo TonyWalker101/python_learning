747a2bf02dbb0f428fa471a782cb46a3ae37ef1d
Tony <awalker780@gmail.com>
2023-01-26 08:13:41 -0500

    imports random module

---
+++ magic8.py
@@ -0,0 +1,10 @@
+#magic 8 ball program that responds to user questions
+import random
+
+name = input("Hello! What is your name?")
+
+question = "Will this program work?"
+
+answer = ""
+
+# print(name)
