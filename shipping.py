145f7355b9b637ece237402f18cb21fb36726f16
Tony <awalker780@gmail.com>
2023-01-27 17:34:14 -0500

    creates premium ground shipping rate

--- shipping.py
+++ shipping.py
@@ -1,6 +1,6 @@
 # program to determine the best package-shipping rate possible
 
-weight = 10
+weight = 8.4
 cost = 0
 
 # ground shipping
@@ -8,12 +8,24 @@ flat_rate = 20.00
 
 if weight <= 2:
   cost = (1.50 * weight) + flat_rate
+  print(cost)
 elif weight <= 6:
   cost = (3.00 * weight) + flat_rate
+  print(cost)
 elif weight <= 10:
   cost = (4.00 * weight) + flat_rate
+  print(cost)
 else:
   cost = (4.75 * weight) + flat_rate
+  print(cost)
+
+# premium group shipping
+
+premium = False
+
+if premium == True:
+  cost = 125.00
+
 
 
 
