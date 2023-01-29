ec4423d4a675c75d9a36f8af2331fc49994c8f82
Tony <awalker780@gmail.com>
2023-01-29 08:56:49 -0500

    creates lists

--- pizza.py
+++ pizza.py
@@ -1,1 +1,10 @@
 # program to practice manipulating lists
+
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+
+num_pizzas = len(toppings)
