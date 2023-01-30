882a5b4a95ae2cb6fada2940a8a42529ab65115b
Tony <awalker780@gmail.com>
2023-01-30 17:45:03 -0500

    finalizes program

--- carlys_clippers.py
+++ carlys_clippers.py
@@ -1,13 +1,14 @@
 # program to practice loops and iteration
 
+# starting lists
 hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
 
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
-
 # finding average price per haircut
+
 total_price = 0
 average_price = 0
 
@@ -21,3 +22,21 @@ print("Average Haircut Price: ", average_price)
 
 new_prices = [price - 5 for price in prices]
 print("New Prices: ", new_prices)
+
+# calculating revenue
+
+total_revenue = 0
+
+for i in range(0, len(hairstyles)):
+  total_revenue += prices[i] * last_week[i]
+print("Total Revenue: ", total_revenue)
+
+# average revenue per day
+
+average_daily_revenue = total_revenue / 7
+print("Average Daily Revenue: ", average_daily_revenue)
+
+# finding haircuts under 30$
+
+cuts_under_30 = [hairstyles[i] for i in range(0, len(new_prices) - 1) if new_prices[i] < 30]
+print("Haircuts under 30$: ", cuts_under_30)
