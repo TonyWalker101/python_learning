90f18c91e447cbffae966c9c1abe453d3362aa6d
Tony <awalker780@gmail.com>
2023-02-01 07:05:08 -0500

    creates fahrenheit to celsius function

--- physics.py
+++ physics.py
@@ -1,1 +1,15 @@
 # program to practice functions
+
+# fahrenheit to celsius
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  return c_temp
+
+f100_in_celcius = f_to_c(100)
+print(f100_in_celcius)
+
+#celsius to fahrenheit
+def c_to_f(c_temp):
+  f_temp = c_temp * 9/5 + 32
+  return f_temp
+
