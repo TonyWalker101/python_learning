7cc07dd92977a088cbfe59e032b171053508c14c
Tony <awalker780@gmail.com>
2023-02-05 21:58:20 -0500

    solves kata

--- boolean_to_string.py
+++ boolean_to_string.py
@@ -1,1 +1,12 @@
 # codewars kata to convert boolean value into string
+
+def boolean_to_string(b):
+    #your code here
+    return str(b)
+  
+# tests
+
+# should print "True"
+print(boolean_to_string(True))
+# should print "False"
+print(boolean_to_string(False))
