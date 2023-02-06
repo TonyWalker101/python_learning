5380a4d2a4ac30ff9721e9d9ed19afa90c031172
Tony <awalker780@gmail.com>
2023-02-06 18:46:04 -0500

    finalizes function

--- get_length.py
+++ get_length.py
@@ -1,1 +1,15 @@
 # function to appreciate len()
+
+def get_length(word):
+  answer = 0
+  for letters in word:
+    answer += 1
+  
+  return answer
+
+# tests
+
+# should print 5
+print(len("Hello"))
+# should print 5
+print(get_length("Hello"))
