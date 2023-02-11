a0f3209775906f1f0028b4fbe7f3d162b91c79f8
Tony <awalker780@gmail.com>
2023-02-11 06:34:49 -0500

    starts function and creates test cases

--- summation.py
+++ summation.py
@@ -1,1 +1,18 @@
 # program to find sum of numbers up to the given number
+
+def summation(num):
+  start = 1
+  answer = 0
+  while start < num:
+    answer += start
+    start += 1
+  return answer
+
+#tests
+
+#should return 1
+print(summation(1))
+#should return 36
+print(summation(8))
+#should return 256
+print(summation(22))
