af18b41ed0eb01e1a1b4938da9023a4e62ae2967
Tony <awalker780@gmail.com>
2023-02-15 08:22:29 -0500

    solves problem

--- non_consecutive.py
+++ non_consecutive.py
@@ -1,1 +1,19 @@
 # program to test if numbers in a list are non-consecutive
+
+def first_non_consecutive(arr):
+  start = arr[0]
+  for i in range(1, len(arr)):
+    if arr[i] - start == 1:
+      start = arr[i]
+    else: 
+      return arr[i]
+  return None
+
+# tests
+
+#should print 6
+print(first_non_consecutive([1,2,3,4,6,7,8]))
+#should print None
+print(first_non_consecutive([1,2,3,4,5,6,7,8]))
+#should print 6
+print(first_non_consecutive([4,6,7,8,9,11]))
