b00f02ec5e650c318311e88918f3a768a803fbb6
Tony <awalker780@gmail.com>
2023-02-16 08:31:34 -0500

    finalizes program

--- the_fender.py
+++ the_fender.py
@@ -1,1 +1,47 @@
-#small program to practice file manipulation
+# small program to practice file manipulation
+import csv
+import json
+
+compromised_users = []
+
+#currently no actual passwords.csv file
+with open("passwords.csv") as password_file:
+  password_csv = csv.DictReader(password_file)
+
+  for row in password_csv:
+    compromised_users.append(row["Username"])
+
+#currently no actual compromised_users.csv file
+with open("compromised_users.txt", "w") as compromised_user_file:
+  for user in compromised_users:
+    compromised_user_file.write(user)
+
+#currently no actual boss_message.json file
+with open("boss_message.json", "w") as boss_message:
+  boss_message_dict = {
+    "recipient": "The Boss",
+    "message": "Mission Success"
+  }
+  json.dump(boss_message_dict, boss_message)
+
+#currently no actual new_passwords.csv file
+with open("new_passwords.csv", "w") as new_passwords_obj:
+  slash_null_sig = ''' 
+ _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/'''
+  
+  new_passwords_obj.write(slash_null_sig)
