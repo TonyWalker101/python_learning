167ee8bf58ca941db0a4f143e22a789c5a7ac954
Tony <awalker780@gmail.com>
2023-02-17 08:39:53 -0500

    solves problem

--- to_jaden_case.py
+++ to_jaden_case.py
@@ -1,19 +1,21 @@
 # program to title case every word in a sentence, like Jaden Smith on Twitter
 
 def to_jaden_case(string):
-# answer = ""
+
   title_string = string.split()
-  print("title_string #1", title_string)
   for i in range(len(title_string)):
-    title_string[i] = title_string[i].title()
+    title_string[i] = title_string[i].capitalize()
   
-  print("title_string #2", title_string)
-  answer = " ".join(title_string)
-  answer.strip()
-  return answer
+  return  " ".join(title_string).strip()
+
+#refactored function using string built-in module
+
+# import string
+
+# def to_jaden_case(sentence):
+#   return string.capwords(sentence)
+
 
 # tests
-
 #should equal "How Can Mirrors Be Real If Our Eyes Aren't Real"
-
 print(to_jaden_case("How can mirrors be real if our eyes aren't real"))
