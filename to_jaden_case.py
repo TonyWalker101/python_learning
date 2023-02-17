cbffe31f12050646d429cd85e539393da4a14b21
Tony <awalker780@gmail.com>
2023-02-17 08:43:10 -0500

    refactors solution

--- to_jaden_case.py
+++ to_jaden_case.py
@@ -1,21 +1,25 @@
-# program to title case every word in a sentence, like Jaden Smith on Twitter
+#program to title case every word in a sentence, like Jaden Smith on Twitter
 
-def to_jaden_case(string):
+#initial solution
+# def to_jaden_case(string):
 
-  title_string = string.split()
-  for i in range(len(title_string)):
-    title_string[i] = title_string[i].capitalize()
+#   title_string = string.split()
+#   for i in range(len(title_string)):
+#     title_string[i] = title_string[i].capitalize()
   
-  return  " ".join(title_string).strip()
+#   return  " ".join(title_string).strip()
 
-#refactored function using string built-in module
+#refactor #1
+def to_jaden_case(string):
+  return  " ".join(word.capitalize() for word in string.split())
 
+#refactor #2 using string built-in module
 # import string
 
 # def to_jaden_case(sentence):
 #   return string.capwords(sentence)
 
 
-# tests
+#tests
 #should equal "How Can Mirrors Be Real If Our Eyes Aren't Real"
 print(to_jaden_case("How can mirrors be real if our eyes aren't real"))
