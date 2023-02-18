218d00cc389aadd803e81746bcf0d1f6672cfd3c
Tony <awalker780@gmail.com>
2023-02-18 18:53:32 -0500

    solves problem

--- double_char.py
+++ double_char.py
@@ -1,1 +1,16 @@
 # program to return a string w/ duplicate letters
+
+def double_char(s):
+  answer = ""
+  for letter in s:
+    answer += letter*2
+  return answer
+
+#tests
+
+#should return "SSttrriinngg"
+print(double_char("String"))
+#should return "HHeelllloo  WWoorrlldd"
+print(double_char("Hello World"))
+#should return "11223344!!__  "
+print(double_char("1234!_ "))
