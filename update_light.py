02d94fb311af1cf9453d38ca0bc0f0d484211310
Tony <awalker780@gmail.com>
2023-02-19 11:51:37 -0500

    solves problem

--- update_light.py
+++ update_light.py
@@ -1,1 +1,17 @@
 #program to update traffic lights to next option
+
+def update_light(current):
+  if current == "green":
+    return "yellow"
+  elif current =="yellow":
+    return "red"
+  return "green"
+
+#tests
+
+#should print 'yellow'
+print(update_light('green'))
+#should print 'red'
+print(update_light('yellow'))
+#should print 'green'
+print(update_light('red'))
