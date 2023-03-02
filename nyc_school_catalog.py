8844128596a001b96e57075156149cece64da0a1
Tony <awalker780@gmail.com>
2023-03-02 07:21:10 -0500

    creates School class

--- nyc_school_catalog.py
+++ nyc_school_catalog.py
@@ -1,2 +1,9 @@
 #small program to practice OOP
 
+class School():
+  def __init__(self, name, level, number_of_students):
+    self.name = name
+    self.level = level
+    self.number_of_students = number_of_students
+  
+  
