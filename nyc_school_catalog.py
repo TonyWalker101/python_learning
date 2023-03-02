7acb2c5083b76f122e7c44f2e99b1abe5c4e6e4d
Tony <awalker780@gmail.com>
2023-03-02 07:25:27 -0500

    creates methods for School class

--- nyc_school_catalog.py
+++ nyc_school_catalog.py
@@ -6,4 +6,20 @@ class School():
     self.level = level
     self.number_of_students = number_of_students
   
+  def get_name(self):
+    return self.name
+
+  def get_level(self):
+    return self.level
   
+  def get_number_of_students(self):
+    return self.number_of_students
+  
+  def set_number_of_students(self, new_number):
+    if int(new_number):
+      self.number_of_students = new_number
+  
+  def __repr__(self):
+    return f"A {self.level} school named {self.name} with {self.number_of_students} students"
+
+#testing School class
