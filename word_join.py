8ce77faa87023e25495964c0e03d604b17ba211c
Tony <awalker780@gmail.com>
2023-03-07 10:36:23 -0500

    finalizes function

--- word_join.py
+++ word_join.py
@@ -1,1 +1,15 @@
 #program to join words from a list together
+
+def smash(words):
+  return " ".join(words)
+
+#tests
+
+#should print "hello world"
+print(smash(["hello", "world"]))
+
+#should print "hello amazing world"
+print(smash(["hello", "amazing", "world"]))
+
+#should print "this is a really long sentence"
+print(smash(["this", "is", "a", "really", "long", "sentence"]))
