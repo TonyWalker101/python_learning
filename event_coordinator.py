fb6f1f6afaf8251b8f6e32d859c98136e7ff3cc8
Tony <awalker780@gmail.com>
2023-03-10 18:46:50 -0500

    initial commit

---
+++ event_coordinator.py
@@ -0,0 +1,14 @@
+#program to practice python function generators
+
+guests = {}
+def read_guestlist(file_name):
+  text_file = open(file_name,'r')
+  while True:
+    line_data = text_file.readline().strip().split(",")
+    if len(line_data) < 2:
+    # If no more lines, close file
+      text_file.close()
+      break
+    name = line_data[0]
+    age = int(line_data[1])
+    guests[name] = age
