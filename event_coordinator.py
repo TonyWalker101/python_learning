9134aaeac3be5c4b3a171dd65087f08c977a45af
Tony <awalker780@gmail.com>
2023-03-12 17:23:37 -0400

    adds logic for .send() feature to function

--- event_coordinator.py
+++ event_coordinator.py
@@ -12,25 +12,31 @@ def read_guestlist(file_name):
     name = line_data[0]
     age = int(line_data[1])
     guests[name] = age
-    yield name
+    n = yield name
+    if n is not None:
+      line_data = n.split(",")
+      name = line_data[0]
+      age = int(line_data[1])
+      guests[name] = age
+      n = guests[name]
 
 
 #creates iterator object w/ function generator
 guestlist = read_guestlist("guests_for_event_coordinator_py.txt")
 
 #section 1, print first 10 guests
-# for i in range(10):
-#   print(next(guestlist))
+for i in range(10):
+  print(next(guestlist))
 
 #section 2, add Jane to guestlist
-# guestlist.send("Jane, 35")
+guestlist.send("Jane, 35")
 
 #section 3, custom loop using next() for all guests in list
-# while True:
-#   try:
-#     print(next(guestlist))
-#   except:
-#     break
+while True:
+  try:
+    print(next(guestlist))
+  except:
+    break
 
 #section 4
 guests_over_21 = (name for name in guests if guests[name] >= 21)
