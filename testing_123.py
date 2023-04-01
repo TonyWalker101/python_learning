faf9f5a6d82130b89ee302b045fa2a658ec51823
Tony <awalker780@gmail.com>
2023-03-31 22:22:16 -0400

    adds test cases

--- testing_123.py
+++ testing_123.py
@@ -3,3 +3,11 @@
 def number(lines):
     
   return [f"{n}: {lines[n-1]}" for n in range(1, len(lines)+1)]
+
+#tests
+
+#should equal []
+print(number([]))
+
+#should equal ["1: a", "2: b", "3: c"]
+print(number(["a", "b", "c"]))
