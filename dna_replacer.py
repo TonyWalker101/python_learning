2847f3afa0fa71bc8d07d428bc417872a2fb8b28
Tony <awalker780@gmail.com>
2023-04-04 21:58:28 -0400

    refactors solution and adds test cases

--- dna_replacer.py
+++ dna_replacer.py
@@ -1,7 +1,17 @@
 #small program to solve string replacement kata
 
 def dna_to_rna(dna):
-  if dna == "":
-      return
-  rna = dna.replace("T", "U")
-  return rna
+  # if dna == "":
+  #     return
+  # rna = dna.replace("T", "U")
+  # return rna
+  return dna.replace("T", "U") if dna != "" else None
+
+#tests
+
+#should equal "UUUU"
+print(dna_to_rna("TTTT"))
+#should equal "GCAU"
+print(dna_to_rna("GCAT"))
+#should equal "GACCGCCGCC"
+print(dna_to_rna("GACCGCCGCC"))
