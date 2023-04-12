af5d574c3d77b777a3efcd86e32212de77b016fa
Tony <awalker780@gmail.com>
2023-04-11 21:49:54 -0400

    finalizes solution

--- base_factor.py
+++ base_factor.py
@@ -1,2 +1,15 @@
 #program to test if base is a factor of specific num
 
+def check_for_factor(base, factor):
+  return True if base % factor == 0 else False
+
+#tests
+
+#should print False
+print(check_for_factor(9, 2))
+#should print False
+print(check_for_factor(653, 7))
+#should print False
+print(check_for_factor(2453, 5))
+#should print False
+print(check_for_factor(24617, 3))
