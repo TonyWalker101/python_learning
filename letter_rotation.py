636f771a5480f09f2a7d86d1bd1151d5124336e7
Tony <awalker780@gmail.com>
2023-04-12 22:18:55 -0400

    finalizes kata

--- letter_rotation.py
+++ letter_rotation.py
@@ -1,22 +1,11 @@
 #kata to move first letter in word around itself
 
 def rotate(str_):
-  #your code here
-  answer = []
-  first_letter = str_[0]
-  current_word = str_
-  start_index = 0
-  end_index = -1
-  for i in range(len(str_)):
-      H_index = current_word.index(first_letter)
-      new_word = str_.replace(first_letter,current_word[H_index+1]
-      
-      new_word[last_index] = str[first_index]
-      answer.append(new_word)
-      index++
-  return answer
+  return [str_[i+1:] + str_[:i+1] for i in range(len(str_))]
 
 #tests
 
 #should return ["231", "312", "123"]
 print(rotate("123"))
+#should return ["elloH", "lloHe", "loHel", "oHell", "Hello"]
+print(rotate("Hello"))
