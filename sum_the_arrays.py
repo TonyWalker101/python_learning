e8f06659c72dd54ebdd69f2ab2ffa83ee12a2993
Tony <awalker780@gmail.com>
2023-04-13 21:56:24 -0400

    solves kata

--- sum_the_arrays.py
+++ sum_the_arrays.py
@@ -1,1 +1,15 @@
 #kata to sum int 
+
+def sum(arr):
+  answer = 0
+  for x in arr:
+    answer += x
+  
+  return answer
+
+#tests
+
+#should return 10
+print(sum([1,2,3,4]))
+#should return 85
+print(sum([10, 28, 14, 33]))
