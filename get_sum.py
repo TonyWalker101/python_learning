1e18cdabcc520b4c64ffb75e3fbbab3d19e00fa8
Tony <awalker780@gmail.com>
2023-04-15 20:57:32 -0400

    refactors solution

--- get_sum.py
+++ get_sum.py
@@ -1,23 +1,25 @@
 #kata to sum between two integers
 
 def get_sum(a,b):
-  answer = 0
-  start = 0
-  end = 0
+  # answer = 0
+  # start = 0
+  # end = 0
   
-  if a > b:
-      start = b
-      end = a
-  elif a < b:
-      start = a
-      end = b
-  elif a == b:
-      return a
+  # if a > b:
+  #     start = b
+  #     end = a
+  # elif a < b:
+  #     start = a
+  #     end = b
+  # elif a == b:
+  #     return a
       
-  for x in range(start,end+1):
-      answer += x
+  # for x in range(start,end+1):
+  #     answer += x
       
-  return answer
+  # return answer
+  
+  return sum(range(min(a,b), max(a,b)+1))
 
 #tests
 
