0418739f0084b305b4c4e1b7cb9f73afd9ea4a8a
Tony <awalker780@gmail.com>
2023-04-16 23:01:11 -0400

    removes comments

--- get_sum.py
+++ get_sum.py
@@ -1,23 +1,6 @@
 #kata to sum between two integers
 
 def get_sum(a,b):
-  # answer = 0
-  # start = 0
-  # end = 0
-  
-  # if a > b:
-  #     start = b
-  #     end = a
-  # elif a < b:
-  #     start = a
-  #     end = b
-  # elif a == b:
-  #     return a
-      
-  # for x in range(start,end+1):
-  #     answer += x
-      
-  # return answer
   
   return sum(range(min(a,b), max(a,b)+1))
 
