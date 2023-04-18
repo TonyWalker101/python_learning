d4ce2837c43577ec1085d0e7104ad51e14334fb3
Tony <awalker780@gmail.com>
2023-04-18 19:06:47 -0400

    initial solution for kata

--- filter_array.py
+++ filter_array.py
@@ -1,1 +1,10 @@
 #kata to find and return number from a string
+
+def filter_string(string):
+  answer = ""
+
+  for letter in string:
+    if isinstance( int(letter),(int,float)):
+      answer += letter
+  
+  return int(answer)
