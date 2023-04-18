461f5bc8b7b1d6ebb3e194df5b02da5b3e0a3128
Tony <awalker780@gmail.com>
2023-04-18 19:08:17 -0400

    adds test cases

--- filter_array.py
+++ filter_array.py
@@ -8,3 +8,16 @@ def filter_string(string):
       answer += letter
   
   return int(answer)
+
+#tests
+
+#should return 123
+print(filter_string("123"))
+#should return 123
+print(filter_string("a1b2c3"))
+#should return 123
+print(filter_string("aa1bb2cc3dd"))
+#should return 1123
+print(filter_string("aa 112 3dd"))
+#should return 112233
+print(filter_string("11bb2c23c3"))
