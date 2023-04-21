43dbf8d078edf1e9f08420c17f0bb9a8274b872c
Tony <awalker780@gmail.com>
2023-04-21 19:21:44 -0400

    starts to implement solution logic

--- find_capitals.py
+++ find_capitals.py
@@ -1,1 +1,15 @@
 #kata to find the capitals in a given string
+def capitals(word):
+  # answer = []
+  # def find_index(lst, lst2):
+  #   for x in lst:
+  #     answer.
+
+  answer_list = filter(lambda x: x.upper() == x, word)
+  # answer_map = map(lambda x: for x in answer_list, answer_list)
+  return list(answer_list)
+
+#tests
+
+#should print [0,3,4,6]
+print( capitals('CodEWaRs'))
