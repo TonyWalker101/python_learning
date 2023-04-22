2d775e6bd48a9c8b820a9b08217a58a8f8e6978b
Tony <awalker780@gmail.com>
2023-04-22 17:16:01 -0400

    refactors solution

--- find_capitals.py
+++ find_capitals.py
@@ -1,9 +1,16 @@
 #kata to find the capitals in a given string
 def capitals(word):
-  def find_index(lst):
-    return word.index(lst)
+  # def find_index(lst):
+  #   return word.index(lst)
 
-  return list(map(find_index, filter(lambda x: x.upper() == x, word)))
+  # return list(map(find_index, filter(lambda x: x.upper() == x, word)))
+
+  answer = []
+  for x in range(len(word)):
+      if word[x] == word[x].upper():
+          answer.append(x)
+            
+  return answer
 
 #tests
 
