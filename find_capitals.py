e604c48e087d2d6db6017d5874f9e968b4fae8c7
Tony <awalker780@gmail.com>
2023-04-22 17:17:11 -0400

    removes comments

--- find_capitals.py
+++ find_capitals.py
@@ -1,10 +1,5 @@
 #kata to find the capitals in a given string
 def capitals(word):
-  # def find_index(lst):
-  #   return word.index(lst)
-
-  # return list(map(find_index, filter(lambda x: x.upper() == x, word)))
-
   answer = []
   for x in range(len(word)):
       if word[x] == word[x].upper():
