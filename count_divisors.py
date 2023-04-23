ea429bd1dc408897d05535dfde56c0be994b140e
Tony <awalker780@gmail.com>
2023-04-23 11:11:39 -0400

    adds test cases

---
+++ count_divisors.py
@@ -0,0 +1,19 @@
+#kata to count the # of divisors for a given num
+
+def divisors(num):
+  pass
+
+#tests
+
+#should print 1
+print(divisors(1)) 
+#should print 3
+print(divisors(4))
+#should print 2
+print(divisors(5))
+#should print 6
+print(divisors(12))
+#should print 8
+print(divisors(30))
+#should print 13
+print(divisors(4096))
