36d75effdbad7014f8e671af2846e81e3cff9527
Tony <awalker780@gmail.com>
2023-04-24 21:19:20 -0400

    adds test cases

--- small_enough.py
+++ small_enough.py
@@ -1,1 +1,13 @@
 #kata to see if numbers in array are lower than given num
+
+def small_enough(array, limit):
+  pass
+
+#tests
+
+#should print True
+print(small_enough([66, 101] ,200))
+#should print False
+print(small_enough([78, 117, 110, 99, 104, 117, 107, 115],100))
+#should print True
+print(small_enough([101, 45, 75, 105, 99, 107], 107))
