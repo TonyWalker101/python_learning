a1f6a9c1c09ce54f2b37de03a87489f9f21e4562
Tony <awalker780@gmail.com>
2023-04-26 16:40:28 -0400

    adds initial function def and test cases

--- list_filtering.py
+++ list_filtering.py
@@ -1,1 +1,13 @@
 #kata to remove the strings from a given list
+
+def filter_list():
+  pass
+
+#tests
+
+#should print [1, 2]
+print(filter_list([1, 2, 'a', 'b']))
+#should print [1, 0, 15]
+print(filter_list([1, 'a', 'b', 0, 15]))
+#should print [1, 2, 123]
+print(filter_list([1, 2, 'aasf', '1', '123']))
