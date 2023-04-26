17cfa9fb2b903e546701493f3f1da85affdeac09
Tony <awalker780@gmail.com>
2023-04-26 16:44:09 -0400

    solves kata

--- list_filtering.py
+++ list_filtering.py
@@ -1,7 +1,7 @@
 #kata to remove the strings from a given list
 
-def filter_list():
-  pass
+def filter_list(lst):
+  return [num for num in lst if isinstance(num, (int, float))]
 
 #tests
 
@@ -10,4 +10,4 @@ print(filter_list([1, 2, 'a', 'b']))
 #should print [1, 0, 15]
 print(filter_list([1, 'a', 'b', 0, 15]))
 #should print [1, 2, 123]
-print(filter_list([1, 2, 'aasf', '1', '123']))
+print(filter_list([1, 2, 'aasf', '1', '123', 123]))
