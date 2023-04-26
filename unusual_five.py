c88e5384163684e8519042f8bba1fd0117c5cbcc
Tony <awalker780@gmail.com>
2023-04-25 22:17:58 -0400

    solves kata

--- unusual_five.py
+++ unusual_five.py
@@ -1,1 +1,9 @@
 #function that always returns 5 but without using int or number characters
+def unusual_five():
+  answer = ["l", "m", "f", "a", "o"]
+  return len(answer)
+
+#tests
+
+#should print 5
+print(unusual_five())
