3ce7e473ce25ac62fa9e5375d220a335ae5d9fe4
Tony <awalker780@gmail.com>
2023-04-27 20:34:27 -0400

    solves kata

--- multiple_divisor.py
+++ multiple_divisor.py
@@ -1,7 +1,12 @@
 #kata to return largest possible divisor given two nums
 
 def max_multiple(x,y):
-  pass
+  answer = []
+  for i in range(1, y+1):
+    if i % x == 0:
+      answer.append(i)
+  
+  return max(answer)
 
 #tests
 
@@ -14,6 +19,6 @@ print(max_multiple(7,17))
 #should print 50
 print(max_multiple(10,50))
 #should print 185
-print(max_multiple(37,2085))
+print(max_multiple(37,200))
 #should print 98
 print(max_multiple(7,100))
