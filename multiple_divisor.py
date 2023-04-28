20cc61edc74e66c4c92f2609e1634239e2c6bcce
Tony <awalker780@gmail.com>
2023-04-27 20:33:01 -0400

    writes test cases

--- multiple_divisor.py
+++ multiple_divisor.py
@@ -1,1 +1,19 @@
 #kata to return largest possible divisor given two nums
+
+def max_multiple(x,y):
+  pass
+
+#tests
+
+#should print 6
+print(max_multiple(2,7))
+#should print 9
+print(max_multiple(3,10))
+#should print 14
+print(max_multiple(7,17))
+#should print 50
+print(max_multiple(10,50))
+#should print 185
+print(max_multiple(37,2085))
+#should print 98
+print(max_multiple(7,100))
