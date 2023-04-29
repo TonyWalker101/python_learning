e934d6e040e69e84a4a5e9e10eaadde3497b945f
Tony <awalker780@gmail.com>
2023-04-29 18:03:02 -0400

    writes function and adds test cases

--- reverse_letter.py
+++ reverse_letter.py
@@ -1,1 +1,19 @@
 #kata to reverse characters in a string while omitting all non-alphabet characters
+
+def reverse_letter(string):
+  answer = ""
+  for i in range(len(string)-1, -1, -1):
+      answer += str(string[i])
+      
+  return answer
+
+#tests
+
+#should print "nahsirk"
+print(reverse_letter("krishan"))
+#should print "nortlu"
+print(reverse_letter("ultr53o?n"))
+#should print "cba"
+print(reverse_letter("ab23c"))
+#should print "nahsirk"
+print(reverse_letter("krish21an"))
