2756d676cb6ba583a2ca41c9146ef4888eaca2d5
Tony <awalker780@gmail.com>
2023-05-01 21:21:41 -0400

    writes unit tests

--- mumbling.py
+++ mumbling.py
@@ -1,1 +1,17 @@
 #kata to emulate mumbling via string manipulation
+
+def accum(word):
+  pass
+
+#tests
+
+#should print "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+print(accum("ZpglnRxqenU"))
+#should print "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+print(accum("NyffsGeyylB"))
+#should print "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+print(accum("MjtkuBovqrU"))
+#should print "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+print(accum("EvidjUnokmM"))
+#should print "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+print(accum("HbideVbxncC"))
