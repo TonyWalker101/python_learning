28f0fedd128653c1fa77c5256220d2a496a4b323
Tony <awalker780@gmail.com>
2023-05-03 16:46:24 -0400

    adds test cases

--- anagram_detection.py
+++ anagram_detection.py
@@ -1,2 +1,17 @@
 #kata to see if one word is an anagram of the other (https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/python)
 
+def is_anagram(test, original):
+  pass
+
+#should print True
+print(is_anagram("foefet", "toffee"))
+#should print True
+print(is_anagram("Buckethead", "DeathCubeK"))
+#should print True
+print(is_anagram("Twoo", "WooT"))
+#should print False
+print(is_anagram("dumble", "bumble"))
+#should print False
+print(is_anagram("ound", "round"))
+#should print False
+print(is_anagram("apple", "pale"))
