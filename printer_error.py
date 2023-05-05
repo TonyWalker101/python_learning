59a50daee0f5e8e5d4efbbdd6941ff1e131c913a
Tony <awalker780@gmail.com>
2023-05-04 20:39:24 -0400

    adds test cases

--- printer_error.py
+++ printer_error.py
@@ -1,2 +1,16 @@
 #codewars kata => https://www.codewars.com/kata/56541980fa08ab47a0000040/train/python
 
+def printer_error(str):
+  pass
+
+#tests
+
+#should print 3/56
+s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+print(printer_error(s))
+#should print 6/60
+s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+print(printer_error(s))
+#should print 11/65
+s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu"
+print(printer_error(s))
