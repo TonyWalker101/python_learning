aa3f671e2aa441672bb59437d5f03bf676bedad1
Tony <awalker780@gmail.com>
2023-05-05 19:07:04 -0400

    add test cases

--- two_to_one.py
+++ two_to_one.py
@@ -1,2 +1,14 @@
 #kata from https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/python
 
+def longest(a1, a2):
+  # your code
+  pass
+
+#tests
+
+#should print "aehrsty"
+print(longest("aretheyhere", "yestheyarehere"))
+#should print "lessdangerousthancoding"
+print(longest("loopingisfunbutdangerous"), "abcdefghilnoprstu")
+#should print "acefghilmnoprstuy"
+print(longest("inmanylanguages", "theresapairoffunctions"))
