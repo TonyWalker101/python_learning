3caf4a1306dd88714f1cb0e807a901d4ff5adf8b
Tony <awalker780@gmail.com>
2023-05-05 19:11:37 -0400

    solves kata

--- two_to_one.py
+++ two_to_one.py
@@ -1,14 +1,24 @@
 #kata from https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/python
 
 def longest(a1, a2):
-  # your code
-  pass
+  answer = []
+
+  for x in a1:
+    if x not in answer:
+      answer.append(x)
+
+  for y in a2:
+    if y not in answer:
+      answer.append(y)
+  
+  answer.sort()
+  return "".join(answer)
 
 #tests
 
 #should print "aehrsty"
 print(longest("aretheyhere", "yestheyarehere"))
-#should print "lessdangerousthancoding"
-print(longest("loopingisfunbutdangerous"), "abcdefghilnoprstu")
+#should print "abcdefghilnoprstu"
+print(longest("loopingisfunbutdangerous", "lessdangerousthancoding"))
 #should print "acefghilmnoprstuy"
 print(longest("inmanylanguages", "theresapairoffunctions"))
