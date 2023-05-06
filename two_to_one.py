25f260159ddd0a15a946bceed36559b5a0201b66
Tony <awalker780@gmail.com>
2023-05-06 13:24:44 -0400

    alternate solution to kata

--- two_to_one.py
+++ two_to_one.py
@@ -1,18 +1,19 @@
 #kata from https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/python
 
 def longest(a1, a2):
-  answer = []
-
-  for x in a1:
-    if x not in answer:
-      answer.append(x)
-
-  for y in a2:
-    if y not in answer:
-      answer.append(y)
   
-  answer.sort()
-  return "".join(answer)
+  def count_letters(*args):
+    results = []
+
+    for word in args:
+      for letter in word:
+        if letter not in results:
+          results.append(letter)
+
+    results.sort()
+    return results
+
+  return "".join(count_letters(a1,a2))
 
 #tests
 
