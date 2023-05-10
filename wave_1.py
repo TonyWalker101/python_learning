9516b19e8c24ecf9ba1962c0771f047a4e825a07
Tony <awalker780@gmail.com>
2023-05-09 22:01:50 -0400

    adds test cases

--- wave_1.py
+++ wave_1.py
@@ -1,1 +1,13 @@
 #codewars kata => https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/train/python
+
+def wave(people):
+  pass
+
+#tests
+
+#should print ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+print(wave("hello"))
+#should print ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"]
+print(wave("codewars"))
+#should print []
+print(wave(""))
