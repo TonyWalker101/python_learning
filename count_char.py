582ddfe62a54c1631a8ffac830c52c54be7953d8
Tony <awalker780@gmail.com>
2023-05-10 16:44:49 -0400

    adds test cases

--- count_char.py
+++ count_char.py
@@ -1,2 +1,17 @@
 # codewares kata => https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/python
 
+def count(s):
+  results = {}
+  if not s:
+    return results
+
+#tests
+
+#should print {'a': 2, 'b': 1}
+print(count('aba'))
+#should print {}
+print(count(''))
+#should print {'a' : 2}
+print(count('aa'))
+#should print {'b' : 2, 'a' : 2}
+print(count('aabb'))
