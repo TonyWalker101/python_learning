51294dec1ca915b2772d0e0d7c89290689366457
Tony <awalker780@gmail.com>
2023-05-12 19:09:28 -0400

    initial solution

--- expanded_number.py
+++ expanded_number.py
@@ -1,7 +1,29 @@
 # codewars kata => https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/python
 
 def expanded_form(num):
-  pass
+  string_num = str(num)
+  results = []
+
+  for i in range(len(string_num)):
+    if string_num[i] == 0:
+      continue
+    elif i == 1:
+      results.append(string_num[i])
+      continue
+    else:
+      num_to_add = string_num[i]
+      count = i
+      while count > 0:
+        num_to_add += "0"
+        count -= 1
+      results.append(num_to_add)
+      # results.append(string_num[i] + str(0 * i))
+  
+  # results.reverse()
+
+  return " + ".join(results)
+      
+
 
 #tests
 
