c8801497810843a7a8e7017e8a6a2590b44f9d9e
Tony <awalker780@gmail.com>
2023-05-12 18:54:00 -0400

    adds test cases

--- expanded_number.py
+++ expanded_number.py
@@ -1,2 +1,13 @@
 # codewars kata => https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/python
 
+def expanded_form(num):
+  pass
+
+#tests
+
+#should print '10 + 2'
+print(expanded_form(12))
+#should print '40 + 2'
+print(expanded_form(42))
+#should print '70000 + 300 + 4'
+print(expanded_form(70304))
