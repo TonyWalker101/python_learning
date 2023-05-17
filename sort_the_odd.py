8f01f694e852748c728424f894337dfdd7eec3a3
Tony <awalker780@gmail.com>
2023-05-17 18:15:48 -0400

    adds test cases

--- sort_the_odd.py
+++ sort_the_odd.py
@@ -1,6 +1,14 @@
 #codewards kata => https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/python
 
 def sort_array(source_array):
-    # Return a sorted array.
-  pass
+  results = []
+  return results
 
+#tests
+
+#should print [1, 3, 2, 8, 5, 4]
+print(sort_array([5, 3, 2, 8, 1, 4]))
+#should print [1, 3, 5, 8, 0]
+print(sort_array([5, 3, 1, 8, 0]))
+#should print []
+print(sort_array([]))
