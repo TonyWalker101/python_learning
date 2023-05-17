97ad408e6c8a06c46dd9dc44efd58308766ad2e8
Tony <awalker780@gmail.com>
2023-05-17 18:29:11 -0400

    solves kata

--- sort_the_odd.py
+++ sort_the_odd.py
@@ -2,6 +2,17 @@
 
 def sort_array(source_array):
   results = []
+  odd_numbers = list(filter(lambda x: x % 2 != 0, source_array))
+  odd_numbers.sort()
+  count = 0
+
+  for num in source_array:
+    if num % 2 == 0:
+      results.append(num)
+    else:
+      results.append(odd_numbers[count])
+      count += 1
+  
   return results
 
 #tests
