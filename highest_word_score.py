e334c41415063cd14697aa2d7dc45b796f83f16d
Tony <awalker780@gmail.com>
2023-05-18 21:27:32 -0400

    adds test cases

--- highest_word_score.py
+++ highest_word_score.py
@@ -3,3 +3,21 @@
 def high(x):
   pass
 
+#tests
+
+#should print 'taxi'
+print(high('man i need a taxi up to ubud'))
+#should print 'volcano'
+print(high('what time are we climbing up the volcano'), 'volcano')
+#should print 'semynak'
+print(high('take me to semynak'), 'semynak')
+#should print 'aa'
+print(high('aa b'), 'aa')
+#should print 'b'
+print(high('b aa'), 'b')
+#should print 'bb'
+print(high('bb d'), 'bb')
+#should print 'd'
+print(high('d bb'), 'd')
+#should print "aaa"
+print(high("aaa b"), "aaa")
