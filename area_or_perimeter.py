6ee27d2db00286e7e9f7bb02c71ef74c94a891b3
Tony <awalker780@gmail.com>
2023-05-19 21:31:22 -0400

    solves kata

--- area_or_perimeter.py
+++ area_or_perimeter.py
@@ -1,18 +1,22 @@
 #codewars kata => https://www.codewars.com/kata/620e839683ba7e003ea49757
 
-def runBF(x,y):
-    
-  pass
+def calc_shape(x):
+  width = x[0]
+  length = x[1]
+
+  return [width*length if width == length else 2*(width+length)]
 
 
 #area calculation tests
+
 #should print ([25]
-print(runBF(([5, 5])))
+print(calc_shape([5, 5]))
 #should print [49]
-print(runBF(([7, 7])))
+print(calc_shape([7, 7]))
 
 #perimeter calculation tests
+
 #should print [18]
-print(runBF(([4, 5])))
+print(calc_shape([4, 5]))
 #should print [24]
-print(runBF(([7, 5])))
+print(calc_shape([7, 5]))
