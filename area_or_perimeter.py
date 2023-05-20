e96e7f98983fbe80ce6d75a5a085178e769c7637
Tony <awalker780@gmail.com>
2023-05-19 21:25:25 -0400

    adds test cases

---
+++ area_or_perimeter.py
@@ -0,0 +1,18 @@
+#codewars kata => https://www.codewars.com/kata/620e839683ba7e003ea49757
+
+def runBF(x,y):
+    
+  pass
+
+
+#area calculation tests
+#should print ([25]
+print(runBF(([5, 5])))
+#should print [49]
+print(runBF(([7, 7])))
+
+#perimeter calculation tests
+#should print [18]
+print(runBF(([4, 5])))
+#should print [24]
+print(runBF(([7, 5])))
