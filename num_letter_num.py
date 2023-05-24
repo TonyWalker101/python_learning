64db11aca4f9a61ee04f1393036f6ad85608af6e
Tony <awalker780@gmail.com>
2023-05-23 21:23:00 -0400

    starts to solve kata

--- num_letter_num.py
+++ num_letter_num.py
@@ -1,7 +1,20 @@
 #codewars kata => https://www.codewars.com/kata/599febdc3f64cd21d8000117/train/python
 
 def numbers_of_letters(n):
-  pass
+  list_num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  results = []
+  pre_total = ""
+
+  #step 1: converting initial arg to 'written number'
+  for digit in str(n):
+    pre_total += list_num[int(digit)]
+    
+  results.append(pre_total)
+
+  #step 2: filling 2nd element in results list
+  results.append(list_num[len(results[0])])
+
+  return results
 
 #should print ["one", "three", "five", "four"
 print(numbers_of_letters(1))
