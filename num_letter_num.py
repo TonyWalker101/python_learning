d37a4b4bef8705028f8aa203716304698be17a96
Tony <awalker780@gmail.com>
2023-05-23 21:14:01 -0400

    adds test cases

--- num_letter_num.py
+++ num_letter_num.py
@@ -1,2 +1,15 @@
 #codewars kata => https://www.codewars.com/kata/599febdc3f64cd21d8000117/train/python
 
+def numbers_of_letters(n):
+  pass
+
+#should print ["one", "three", "five", "four"
+print(numbers_of_letters(1))
+#should print ["onetwo", "six", "three", "five", "four"]
+print(numbers_of_letters(12))
+#should print ["threeseven", "onezero", "seven", "five", "four"]
+print(numbers_of_letters(37))
+#should print ['threeoneone', 'oneone', 'six', 'three', 'five', 'four']
+print(numbers_of_letters(311))
+#should print ["nineninenine", "onetwo", "six", "three", "five", "four"]
+print(numbers_of_letters(999))
