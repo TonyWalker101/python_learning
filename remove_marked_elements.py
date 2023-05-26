5fa5f5ee9fa3cdcb30a33d2afe35caec36b77dc4
Tony <awalker780@gmail.com>
2023-05-25 21:01:36 -0400

    adds test cases

--- remove_marked_elements.py
+++ remove_marked_elements.py
@@ -1,2 +1,21 @@
 #codewards kata => https://www.codewars.com/kata/563089b9b7be03472d00002b/train/python
 
+def remove_(integer_list, values_list):
+  return []
+
+#tests
+
+#should print  [2, 2, 4]
+integer_list =  [1, 1, 2 ,3 ,1 ,2 ,3 ,4]
+values_list = [1, 3]
+print(remove_(integer_list, values_list))
+
+#should print [5, 6 ,7 ,8]
+integer_list = [1, 1, 2 ,3 ,1 ,2 ,3 ,4, 4, 3 ,5, 6, 7, 2, 8]
+values_list  = [1, 3, 4, 2]
+print(remove_(integer_list, values_list))
+
+#should print [8, 7, 6, 5, 1]
+integer_list = [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2 , 3]
+values_list  = [2, 4, 3]
+print(remove_(integer_list, values_list))
