ac7d102754033468625f23507ac2594f547a786f
Tony <awalker780@gmail.com>
2023-05-27 15:00:36 -0400

    solves kata

--- count_duplicates.py
+++ count_duplicates.py
@@ -1,7 +1,15 @@
 #codewars kata => https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/python
 
+from collections import Counter
+
 def duplicate_count(text):
-  pass
+  text_formatted = Counter(text.lower())
+  results = [letter for letter,count in text_formatted.items() if count > 1]
+  
+  return len(results)
+
+
+
 
 #should print 0 
 print(duplicate_count(""))
