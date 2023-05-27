9b31e3e02def4ae54542bbf8ec6ca79a6a1ac845
Tony <awalker780@gmail.com>
2023-05-27 14:52:29 -0400

    adds test cases

--- count_duplicates.py
+++ count_duplicates.py
@@ -1,2 +1,15 @@
 #codewars kata => https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/python
 
+def duplicate_count(text):
+  pass
+
+#should print 0 
+print(duplicate_count(""))
+#should print 0
+print(duplicate_count("abcde"))
+#should print 1
+print(duplicate_count("abcdeaa"))
+#should print 2
+print(duplicate_count("abcdeaB"))
+#should print 2
+print(duplicate_count("Indivisibilities"))
