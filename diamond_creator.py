ce2d2a86c2dce756718c0641f9b0f571de6d7484
Tony <awalker780@gmail.com>
2023-05-29 20:09:54 -0400

    initial solution to kata

--- diamond_creator.py
+++ diamond_creator.py
@@ -1,5 +1,17 @@
 #codewars kata => https://www.codewars.com/kata/5503013e34137eeeaa001648/python
 
 def diamond(n):
-  pass
+  if n > 0 and n % 2 == 1:
+      diamond = ""
+      for i in range(n):
+          diamond += " " * abs((n/2) - i)
+          diamond += "*" * (n - abs((n-1) - 2 * i))
+          diamond += "\n"
+      return diamond
+  else:
+      return None
+  
+#tests
+
+diamond(5)
 
