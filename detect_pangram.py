db56083288371e9182b01f89aceb0f6e58f8e7ff
Tony <awalker780@gmail.com>
2023-05-31 20:48:37 -0400

    initial commit

---
+++ detect_pangram.py
@@ -0,0 +1,11 @@
+#codewars kata => https://www.codewars.com/kata/545cedaa9943f7fe7b000048/python
+
+def is_pangram(s):
+  return False
+
+#tests
+
+#should print True
+print(is_pangram("The quick, brown fox jumps over the lazy dog!"))
+#should print False
+print(is_pangram("1bcdefghijklmnopqrstuvwxyz"))
