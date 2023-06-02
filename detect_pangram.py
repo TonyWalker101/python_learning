b1873a4be9ebde1d47126f9f8beda6a98f013c28
Tony <awalker780@gmail.com>
2023-06-01 21:52:25 -0400

    solves kata

--- detect_pangram.py
+++ detect_pangram.py
@@ -1,7 +1,18 @@
 #codewars kata => https://www.codewars.com/kata/545cedaa9943f7fe7b000048/python
+from collections import Counter
 
 def is_pangram(s):
-  return False
+  all_letters = Counter(s.lower())
+  count = 0
+
+  for key in all_letters.keys():
+    if key.isalpha():
+      count += 1
+  
+  if count != 26:
+    return False
+  
+  return True
 
 #tests
 
