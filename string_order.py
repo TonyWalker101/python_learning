b8f95eecb497261fccb49473a20c0fbb6fa089dc
Tony <awalker780@gmail.com>
2023-06-02 14:25:31 -0400

    adds test cases

--- string_order.py
+++ string_order.py
@@ -1,2 +1,11 @@
 #codewars kata => https://www.codewars.com/kata/55c45be3b2079eccff00010f/python
 
+def order(sentence):
+  return
+
+#should print "Thi1s is2 3a T4est"
+print(order("is2 Thi1s T4est 3a"))
+#should print "Fo1r the2 g3ood 4of th5e pe6ople"
+print(order("4of Fo1r pe6ople g3ood th5e the2"))
+#should print ""
+print(order(""))
