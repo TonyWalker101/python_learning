6335353b20bb526a38644cd8dbcd33e801bd9b8e
Tony <awalker780@gmail.com>
2023-06-04 21:32:16 -0400

    initial commit

---
+++ vowel_code.py
@@ -0,0 +1,18 @@
+#codewars kata => https://www.codewars.com/kata/53697be005f803751e0015aa
+
+def encode(st):
+  return
+    
+def decode(st):
+  return
+
+#tests
+
+#should print 'h2ll4'
+print(encode('hello'))
+#should print 'H4w 1r2 y45 t4d1y?'
+print(encode('How are you today?'))
+#should print 'Th3s 3s 1n 2nc4d3ng t2st.'
+print(encode('This is an encoding test.'))
+#should print 'hello'
+print(decode('h2ll4'))
