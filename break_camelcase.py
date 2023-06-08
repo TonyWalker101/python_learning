766034d11baa1f186ff8946cfc22bf7dc023765c
Tony <awalker780@gmail.com>
2023-06-07 23:31:00 -0400

    initial commit

---
+++ break_camelcase.py
@@ -0,0 +1,13 @@
+#codewars kata => https://www.codewars.com/kata/5208f99aee097e6552000148/python
+
+def solution(s):
+  pass
+
+#tests
+
+#should print "hello World"
+print(solution("helloWorld"))
+#should print "camel Case"
+print(solution("camelCase"))
+#should print "break Camel Case"
+print(solution("breakCamelCase"))
