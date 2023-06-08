64760fe29acfe2b8bad0353616a5215368844bb0
Tony <awalker780@gmail.com>
2023-06-08 08:29:15 -0400

    refactors solution

--- break_camelcase.py
+++ break_camelcase.py
@@ -1,13 +1,18 @@
 #codewars kata => https://www.codewars.com/kata/5208f99aee097e6552000148/python
 
 def solution(s):
-  results = ""
-  for letter in s:
-    if letter.isupper():
-      results += " "
-    results += letter
+    
+  # results = ""
+  # for letter in s:
+  #   if letter.isupper():
+  #     results += " "
+  #   results += letter
   
-  return results
+  # return results
+
+  #refactored solution
+    return  "".join([" " + letter if letter.isupper() else letter for letter in s ])
+    
 
 #tests
 
