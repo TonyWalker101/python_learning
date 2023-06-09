18dc13a2db2ea7962862c50a3e4f0313674dea65
Tony <awalker780@gmail.com>
2023-06-09 17:31:30 -0400

    refactors solution

--- find_odd_int.py
+++ find_odd_int.py
@@ -3,8 +3,13 @@
 from collections import Counter
 
 def find_it(seq):
-  seq_count = Counter(seq)
-  return int(" ".join([str(key) for key, val in seq_count.items() if val % 2 != 0]))
+  # seq_count = Counter(seq)
+  # return int(" ".join([str(key) for key, val in seq_count.items() if val % 2 != 0]))
+
+  #refactored solution
+  for i in seq:
+    if seq.count(i) % 2 != 0:
+      return i
 
 #should print 5
 print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
