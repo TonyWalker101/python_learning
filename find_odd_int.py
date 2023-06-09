9637607c941b568a9e4e94d7bbce6f44a668a835
Tony <awalker780@gmail.com>
2023-06-09 17:21:51 -0400

    initial commit

---
+++ find_odd_int.py
@@ -0,0 +1,25 @@
+#codewars kata => https://www.codewars.com/trainer/python
+
+def find_it(seq):
+  return None
+
+#should print 5
+print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+
+#should print -1
+print(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]))
+
+#should print 5
+print(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]))
+
+#should print 10
+print(find_it([10]), 10)
+
+#should print 10
+print(find_it([10, 10, 10])) 
+       
+#should print 10
+print(find_it([1,1,1,1,1,1,10,1,1,1,1]))
+
+#should print 1
+print(find_it([5,4,3,2,1,5,4,3,2,10,10]))
