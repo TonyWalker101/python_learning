ff4dcffa81bcb020e1971c865e5a56c5e438fac7
Tony <awalker780@gmail.com>
2023-06-16 21:52:00 -0400

    adds test cases

--- remove_paran.py
+++ remove_paran.py
@@ -1,1 +1,19 @@
 #codewars kata => https://www.codewars.com/kata/5f7c38eb54307c002a2b8cc8
+
+def remove_parentheses(s):
+  pass
+
+#tests
+
+#should print "exampleexample"
+print(remove_parentheses("example(unwanted thing)example"))
+#should print "example  example"
+print(remove_parentheses("example (unwanted thing) example"))
+#should print "a e"
+print(remove_parentheses("a (bc d)e"))
+#should print "a"
+print(remove_parentheses("a(b(c))"))
+#should print "hello example  something"
+print(remove_parentheses("hello example (words(more words) here) something"))
+#should print "  "
+print(remove_parentheses("(first group) (second group) (third group)"))
