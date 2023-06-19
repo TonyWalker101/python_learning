8d732e122e72e141dd8b5963a2789906a3b51b29
Tony <awalker780@gmail.com>
2023-06-18 20:52:58 -0400

    adds test cases

--- parse_int.py
+++ parse_int.py
@@ -1,2 +1,15 @@
 #codewars kata => https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/python
 
+def get_age(age):
+  pass
+
+#tests
+
+#should print 2
+print(get_age("2 years old"))
+#should print 4
+print(get_age("4 years old"))
+#should print 5
+print(get_age("5 years old"))
+#should print 7
+print(get_age("7 years old"))
