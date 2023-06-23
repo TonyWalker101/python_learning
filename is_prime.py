12a83e6bb46b5ffe613b579121dde4360a745b64
Tony <awalker780@gmail.com>
2023-06-22 20:42:05 -0400

    adds test cases

--- is_prime.py
+++ is_prime.py
@@ -1,1 +1,19 @@
 #codewars kata => https://www.codewars.com/kata/5262119038c0985a5b00029f/python
+
+def is_prime(num):
+  pass
+
+#tests
+
+#should print True
+print(is_prime(3))
+#should print True
+print(is_prime(5))
+#should print True
+print(is_prime(7))
+#should print False
+print(is_prime(4))
+#should print False
+print(is_prime(6))
+#should print False
+print(is_prime(8))
