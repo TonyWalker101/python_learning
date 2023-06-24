00d4df6163115d1545bc920849c853de79201a93
Tony <awalker780@gmail.com>
2023-06-23 23:14:45 -0400

    solves kata

--- is_prime.py
+++ is_prime.py
@@ -1,7 +1,19 @@
 #codewars kata => https://www.codewars.com/kata/5262119038c0985a5b00029f/python
 
+from math import sqrt
+
 def is_prime(num):
-  pass
+  if num < 1:
+    return False
+  
+  i = 2
+  while i <= sqrt(num):
+    if num % i == 0:
+      return False
+    i += 1
+
+  return True
+
 
 #tests
 
