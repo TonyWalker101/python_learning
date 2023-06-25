0cfe4a491b9d151c02db50855d62226cd99e6a28
Tony <awalker780@gmail.com>
2023-06-24 22:21:23 -0400

    adds test cases

--- returning_string.py
+++ returning_string.py
@@ -1,2 +1,11 @@
 #codewars kata => https://www.codewars.com/kata/55a70521798b14d4750000a4/python
 
+def greet(name):
+  pass
+
+#tests
+
+#should print "Hello, Ryan how are you doing today?"
+print(greet('Ryan'))
+#should print "Hello, Shingles how are you doing today?"
+print(greet('Shingles'))
