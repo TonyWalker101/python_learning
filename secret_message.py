7b67debab78b8dc48085aee096729fd5187f358c
Tony <awalker780@gmail.com>
2023-06-27 22:50:11 -0400

    adds test cases

--- secret_message.py
+++ secret_message.py
@@ -1,2 +1,13 @@
 #codewars kata => https://www.codewars.com/kata/54808e45ab03a2c8330009fb
 
+def find_secret_message(paragraph):
+  pass
+
+#tests
+
+#should print 'this test is'
+paragraph = 'This is a test. this test is fun.'
+print(find_secret_message(paragraph))
+#should print 'zxcv asdf qwer'
+paragraph = 'asdf qwer zxcv. zxcv fdsa rewq. qazw asdf sxed. qwer crfv.'
+print(find_secret_message(paragraph))
