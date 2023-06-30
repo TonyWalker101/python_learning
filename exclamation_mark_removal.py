7ee7ca7060dc7329cfde95494b5e847dff382096
Tony <awalker780@gmail.com>
2023-06-29 22:24:43 -0400

    refactors solution

--- exclamation_mark_removal.py
+++ exclamation_mark_removal.py
@@ -1,19 +1,15 @@
 #codewars kata => https://www.codewars.com/kata/57fae964d80daa229d000126/python
 
 def remove(s):
-  pass
+  return s[:-1] if s and s[-1] == "!" else s
 
 #tests
 
-tests = [
-  #[input, [expected]],
-  ["Hi!", "Hi"],
-  ["Hi!!!","Hi!!"],
-  ["!Hi", "!Hi"],
-  ["!Hi!", "!Hi"],
-  ["Hi! Hi!", "Hi! Hi"],
-  ["Hi", "Hi"],
-]
-
-for inp, exp in tests:
-  print(remove(inp), exp)
+#Should print Hi
+print(remove("Hi!"))
+#Should print Hi!!
+print(remove("Hi!!!"))
+#Should print Hi
+print(remove("Hi"))
+#Should print Hi! Hi
+print(remove("Hi! Hi!"))
