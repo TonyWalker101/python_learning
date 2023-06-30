0394e099139e1127ec8f147eb21b62c75c780a58
Tony <awalker780@gmail.com>
2023-06-29 22:14:23 -0400

    adds test cases

--- exclamation_mark_removal.py
+++ exclamation_mark_removal.py
@@ -1,3 +1,19 @@
 #codewars kata => https://www.codewars.com/kata/57fae964d80daa229d000126/python
 
- 
+def remove(s):
+  pass
+
+#tests
+
+tests = [
+  #[input, [expected]],
+  ["Hi!", "Hi"],
+  ["Hi!!!","Hi!!"],
+  ["!Hi", "!Hi"],
+  ["!Hi!", "!Hi"],
+  ["Hi! Hi!", "Hi! Hi"],
+  ["Hi", "Hi"],
+]
+
+for inp, exp in tests:
+  print(remove(inp), exp)
