0cc3f4b9468c28b06043561cabc4c5f316d0af75
Tony <awalker780@gmail.com>
2023-06-30 21:32:50 -0400

    initial commit

---
+++ cat_dog_years.py
@@ -0,0 +1,13 @@
+#codewars kata => https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/python
+
+def human_years_cat_years_dog_years(human_years):
+  return [0,0,0]
+
+#tests
+
+#should print [1,15,15]
+print(human_years_cat_years_dog_years(1))
+#should print [2,24,24]
+print(human_years_cat_years_dog_years(2))
+#should print [10,56,64
+print(human_years_cat_years_dog_years(10))
