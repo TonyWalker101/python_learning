30b4e165024c0934a49fecaf4433fbe3dc4f60f8
Tony <awalker780@gmail.com>
2023-07-02 22:18:33 -0400

    solves kata

--- cat_dog_years.py
+++ cat_dog_years.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/python
 
-def human_years_cat_years_dog_years(human_years):
-  return [0,0,0]
+def human_years_cat_years_dog_years(x):
+  return [x, 24+(x-2)*4 if (x != 1) else 15, 24+(x-2)*5 if (x != 1) else 15]
 
 #tests
 
