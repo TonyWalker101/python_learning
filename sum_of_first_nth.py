7dfdf314bfd85348f6316865049d141d158bd7ef
Tony <awalker780@gmail.com>
2023-07-04 15:19:19 -0400

    adds test cases

--- sum_of_first_nth.py
+++ sum_of_first_nth.py
@@ -1,2 +1,13 @@
 #codewars kata => https://www.codewars.com/kata/555eded1ad94b00403000071/python
 
+def series_sum(n):
+  pass
+
+#tests
+
+#should print "1.00"
+print(series_sum(1))
+#should print "1.25"
+print(series_sum(2))
+#should print "1.39"
+print(series_sum(3))
