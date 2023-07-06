fb641379357864c7277cd0babf2bebf1a7471a58
Tony <awalker780@gmail.com>
2023-07-05 22:48:23 -0400

    initial commit

---
+++ between_start_end.py
@@ -0,0 +1,11 @@
+#codewars kata => https://www.codewars.com/kata/55ecd718f46fba02e5000029/python
+
+def between(a,b):
+  pass
+
+#tests
+
+#should print [1, 2, 3, 4] 
+print(between(1, 4))
+#should print [-2, -1, 0, 1, 2]
+print(between(-2, 2))
