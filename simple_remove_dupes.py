2bf720ab6fe73437053f0fb759ad1abd0d30261a
Tony <awalker780@gmail.com>
2023-07-07 19:51:24 -0400

    adds test cases

--- simple_remove_dupes.py
+++ simple_remove_dupes.py
@@ -1,2 +1,15 @@
 #codewars kata => https://www.codewars.com/kata/5ba38ba180824a86850000f7
 
+def solve(arr): 
+  pass
+
+#tests
+
+#should print [4,6,3]
+print(solve([3,4,4,3,6,3]))
+#should print [1,2,3]
+print(solve([1,2,1,2,1,2,3]))
+#should print [1,2,3,4]
+print(solve([1,2,3,4]),)
+#should print [4,5,2,1]
+print(solve([1,1,4,5,1,2,1]))
