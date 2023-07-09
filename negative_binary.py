94e60e5bd208902c6398fb0cf0a1ff2f3a80f900
Tony <awalker780@gmail.com>
2023-07-09 07:04:51 -0400

    adds test cases

--- negative_binary.py
+++ negative_binary.py
@@ -1,2 +1,19 @@
 #codewars kata => https://www.codewars.com/kata/5becace7063291bebf0001d5/python
 
+def positive_to_negative(binary):
+  return
+
+#tests
+
+#should print [1, 0, 1, 1, 1]
+print(positive_to_negative([0, 1, 0, 0, 1]))
+#should print [0, 0, 0, 0]
+print(positive_to_negative([0, 0, 0, 0]))
+#should print [1, 1, 1, 0]
+print(positive_to_negative([0, 0, 1, 0]))
+#should print [1, 1, 0, 1]
+print(positive_to_negative([0, 0, 1, 1]))
+#should print [1, 1, 0, 0]
+print(positive_to_negative([0, 1, 0, 0]))
+#should print [1, 1, 0, 0]
+print(positive_to_negative([0, 1, 0, 0]))
