eec8827f058c9cefa5f4be59bdc9b9f00c857630
Tony <awalker780@gmail.com>
2023-07-09 07:26:23 -0400

    implements first portion of solution

--- negative_binary.py
+++ negative_binary.py
@@ -1,7 +1,25 @@
 #codewars kata => https://www.codewars.com/kata/5becace7063291bebf0001d5/python
 
 def positive_to_negative(binary):
-  return
+  flipped_binary = list(map(lambda x: 0 if x == 1 else 1, binary))
+  results = []
+  # print(binary)
+
+  if flipped_binary[-1] == 0:
+    flipped_binary = flipped_binary[:-1]
+    flipped_binary.append(1)
+    return flipped_binary
+  
+  # for i in range(len(flipped_binary)-1, 0):
+  #   if flipped_binary(i) == 0:
+  #     flipped_binary = flipped_binary[:i] + flipped_binary[i:]
+  #     flipped_binary.insert(i, 1)
+
+
+  
+
+  # print(flipped_binary)
+  # return
 
 #tests
 
