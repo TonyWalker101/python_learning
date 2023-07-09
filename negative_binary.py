fb5a31f40de9eef61e79dfab3149326549ee5ea1
Tony <awalker780@gmail.com>
2023-07-09 07:46:41 -0400

    solves kata

--- negative_binary.py
+++ negative_binary.py
@@ -1,26 +1,25 @@
 #codewars kata => https://www.codewars.com/kata/5becace7063291bebf0001d5/python
 
 def positive_to_negative(binary):
+  #if binary num = 0, return original binary list
+  if binary.count(1) == 0:
+    return binary
+  
+  #inverts (flips) binary nums from binary list
   flipped_binary = list(map(lambda x: 0 if x == 1 else 1, binary))
-  results = []
-  # print(binary)
 
-  if flipped_binary[-1] == 0:
-    flipped_binary = flipped_binary[:-1]
-    flipped_binary.append(1)
-    return flipped_binary
-  
-  # for i in range(len(flipped_binary)-1, 0):
-  #   if flipped_binary(i) == 0:
-  #     flipped_binary = flipped_binary[:i] + flipped_binary[i:]
-  #     flipped_binary.insert(i, 1)
+  #starts loop at end of list
+  for i in range(len(flipped_binary)-1,0,-1):
+    #add 1 if num is 0, then stop iteration
+    if flipped_binary[i] == 0:
+      flipped_binary = flipped_binary[:i] + [1] + flipped_binary[i+1:]
+      break
+    else:
+      #if num is 1, make 0 and continue to next num in list
+      flipped_binary = flipped_binary[:i] + [0] + flipped_binary[i+1:]
 
-
-  
-
-  # print(flipped_binary)
-  # return
-
+  return flipped_binary
+ 
 #tests
 
 #should print [1, 0, 1, 1, 1]
