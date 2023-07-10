e2ba72462ad610c5b42b8d9d88fe98378ed7bce2
Tony <awalker780@gmail.com>
2023-07-10 07:57:40 -0400

    adds test cases

--- sum_digits.py
+++ sum_digits.py
@@ -1,2 +1,13 @@
 #codewars kata => https://www.codewars.com/kata/52f3149496de55aded000410/python
 
+def sum_digits(number):
+  pass
+
+#tests
+
+#should print 1
+print(sum_digits(10))
+#should print 18
+print(sum_digits(99))
+#should print 5
+print(sum_digits(-32))
