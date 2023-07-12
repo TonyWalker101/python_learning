70af433c23ea759f20cb015bb0131ebf9d7c22df
Tony <awalker780@gmail.com>
2023-07-12 08:15:43 -0400

    adds test cases

--- alpha_sequence.py
+++ alpha_sequence.py
@@ -1,2 +1,19 @@
 #codewars kata => https://www.codewars.com/kata/5bd00c99dbc73908bb00057a
 
+def alpha_seq(string):
+  return
+
+#tests
+
+#should print "A,Bb,Ccc,Ffffff,Ffffff"
+print(alpha_seq("BfcFA"))
+#should print "Eeeee,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Nnnnnnnnnnnnnn,Pppppppppppppppp,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Uuuuuuuuuuuuuuuuuuuuu,Xxxxxxxxxxxxxxxxxxxxxxxx,Zzzzzzzzzzzzzzzzzzzzzzzzzz"
+print(alpha_seq("ZpglnRxqenU"))
+#should print "Bb,Eeeee,Ffffff,Ffffff,Ggggggg,Llllllllllll,Nnnnnnnnnnnnnn,Sssssssssssssssssss,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy,Yyyyyyyyyyyyyyyyyyyyyyyyy"
+print(alpha_seq("NyffsGeyylB"))
+#should print "Bb,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Ooooooooooooooo,Qqqqqqqqqqqqqqqqq,Rrrrrrrrrrrrrrrrrr,Tttttttttttttttttttt,Uuuuuuuuuuuuuuuuuuuuu,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv"
+print(alpha_seq("MjtkuBovqrU"))
+#should print "Dddd,Eeeee,Iiiiiiiii,Jjjjjjjjjj,Kkkkkkkkkkk,Mmmmmmmmmmmmm,Mmmmmmmmmmmmm,Nnnnnnnnnnnnnn,Ooooooooooooooo,Uuuuuuuuuuuuuuuuuuuuu,Vvvvvvvvvvvvvvvvvvvvvv"
+print(alpha_seq("EvidjUnokmM"))
+#should print "Bb,Bb,Ccc,Ccc,Dddd,Eeeee,Hhhhhhhh,Iiiiiiiii,Nnnnnnnnnnnnnn,Vvvvvvvvvvvvvvvvvvvvvv,Xxxxxxxxxxxxxxxxxxxxxxxx"
+print(alpha_seq("HbideVbxncC"))
