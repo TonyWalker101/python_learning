93e2593031f49614909162f278067827fcbf2ef4
Tony <awalker780@gmail.com>
2023-07-14 12:08:03 -0400

    creates test cases

--- you_are_a_cube.py
+++ you_are_a_cube.py
@@ -3,3 +3,15 @@
 def you_are_a_cube(cube):
   pass
 
+#tests
+
+#should print True
+print(you_are_a_cube(27))
+#should print True
+print(you_are_a_cube(1))
+#should print False
+print(you_are_a_cube(2))
+#should print False
+print(you_are_a_cube(99))
+#should print True
+print(you_are_a_cube(64))
