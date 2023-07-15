358bc72686a3b056166a96b835fe03edcd0b99e5
Tony <awalker780@gmail.com>
2023-07-15 08:25:40 -0400

    adds test cases

--- no_zeroes_here.py
+++ no_zeroes_here.py
@@ -1,2 +1,19 @@
 #codewars kata => https://www.codewars.com/kata/570a6a46455d08ff8d001002/python
 
+def no_boring_zeros(n):
+  pass
+
+#tests
+
+#should print 145
+print(no_boring_zeros(1450))
+#should print 96
+print(no_boring_zeros(960000))
+#should print 105
+print(no_boring_zeros(1050))
+#should print -105
+print(no_boring_zeros(-1050))
+#should print 0
+print(no_boring_zeros(0))
+#should print 2016
+print(no_boring_zeros(2016))
