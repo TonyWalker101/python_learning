51023e54ba9c54895d7a2d3ba3271618eb45c652
Tony <awalker780@gmail.com>
2023-07-19 22:10:29 -0400

    adds test cases

--- sum_sequence.py
+++ sum_sequence.py
@@ -2,3 +2,26 @@
 
 def sequence_sum(begin_number, end_number, step):
   pass
+
+#tests
+
+#should print 12
+print(sequence_sum(2, 6, 2))
+#should print 15
+print(sequence_sum(1, 5, 1))
+#should print 5
+print(sequence_sum(1, 5, 3))
+#should print 45
+print(sequence_sum(0, 15, 3))
+#should print 0
+print(sequence_sum(16, 15, 3))
+#should print 26
+print(sequence_sum(2, 24, 22))
+#should print 2
+print(sequence_sum(2, 2, 2))
+#should print 2
+print(sequence_sum(2, 2, 1))
+#should print 35
+print(sequence_sum(1, 15, 3))
+#should print 0
+print(sequence_sum(15, 1, 3))
