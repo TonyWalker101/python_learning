0815d05ac2375652d4c1d1f535c722f25a359d03
Tony <awalker780@gmail.com>
2023-07-21 21:49:15 -0400

    adds test cases

--- unique_number.py
+++ unique_number.py
@@ -1,2 +1,18 @@
 #codewars kata => https://www.codewars.com/kata/585d7d5adb20cf33cb000235/python
 
+from collections import Counter
+
+def find_uniq(arr):
+  counted_arr = Counter(arr)
+  print(counted_arr)
+  
+  # return "".join([x for x in counted_arr if x == 1])
+
+#tests
+
+#should print 2
+print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
+#should print 0.55
+print(find_uniq([ 0, 0, 0.55, 0, 0 ]))
+#should print 10
+print(find_uniq([ 3, 10, 3, 3, 3 ]))
