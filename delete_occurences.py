60b7baac1682d49ea5dc596ec076e3cdd08ffb1e
Tony <awalker780@gmail.com>
2023-07-22 22:25:30 -0400

    initial commit

---
+++ delete_occurences.py
@@ -0,0 +1,12 @@
+#codewars kata => https://www.codewars.com/kata/554ca54ffa7d91b236000023/python
+
+def delete_nth(order,max_e):
+  pass
+
+#tests
+
+#should print [20,37,21]
+print(delete_nth([20,37,20,21], 1))
+#should print [1, 1, 3, 3, 7, 2, 2, 2]
+print(delete_nth([1,1,3,3,7,2,2,2,2], 3))
+
