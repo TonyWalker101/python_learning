f7506710ba7da8197795b617644690d5b42a2804
Tony <awalker780@gmail.com>
2023-07-24 23:47:33 -0400

    initial commit

---
+++ unique_order.py
@@ -0,0 +1,11 @@
+#codewars kata => https://www.codewars.com/kata/54e6533c92449cc251001667/python
+
+def unique_in_order(sequence):
+  return
+
+#tests
+
+#should print [1, 2, 3, -1]
+print(unique_in_order([1, 2, 3, 3, -1]))
+#should print ["a", "b", "a"]
+print(unique_in_order(["a", "b", "b", "a"]))
