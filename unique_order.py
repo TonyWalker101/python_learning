bd8338a565f9e7b5b2e42b74f028309ae8283bda
Tony <awalker780@gmail.com>
2023-07-24 23:52:01 -0400

    initial solution

--- unique_order.py
+++ unique_order.py
@@ -1,7 +1,15 @@
 #codewars kata => https://www.codewars.com/kata/54e6533c92449cc251001667/python
 
 def unique_in_order(sequence):
-  return
+  results = []
+  for i in range(len(sequence)):
+    if i == 0:
+      results.append(sequence[i])
+    
+    if results[i-1] != sequence[i]:
+      results.append(sequence[i])
+
+  return results
 
 #tests
 
