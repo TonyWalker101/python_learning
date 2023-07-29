28a8f306eadf049a75757ba504d32c467392f48d
Tony <awalker780@gmail.com>
2023-07-28 23:13:54 -0400

    adds test cases

--- is_plaindrome.py
+++ is_plaindrome.py
@@ -3,3 +3,19 @@
 def is_palindrome(s):
   return
 
+#tests
+
+#should print True
+print(is_palindrome('a'), True)
+#should print True
+print(is_palindrome('aba'), True)
+#should print True
+print(is_palindrome('Abba'), True)
+#should print True
+print(is_palindrome('malam'), True)
+#should print False
+print(is_palindrome('walter'), False)
+#should print True
+print(is_palindrome('kodok'), True)
+#should print False
+print(is_palindrome('Kasue'), False)
