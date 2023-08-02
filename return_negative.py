a67eef4267c323bf11d164d65664c892e7a9c20b
Tony <awalker780@gmail.com>
2023-08-01 21:44:05 -0400

    solves kata

--- return_negative.py
+++ return_negative.py
@@ -1,5 +1,8 @@
 #codewars kata => https://www.codewars.com/kata/55685cd7ad70877c23000102/python
 
 def make_negative( number ):
-  pass
+  if number == 0:
+    return 0
+  
+  return -number if number > 0 else number 
 
