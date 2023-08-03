19e1e5105f35888cd5c0bdf0a0e015530ba31c86
Tony <awalker780@gmail.com>
2023-08-02 22:21:13 -0400

    solves kata

---
+++ under_pressure.py
@@ -0,0 +1,17 @@
+#codewars kata => https://www.codewars.com/kata/53ee5429ba190077850011d4/python
+
+def double_integer(i):
+  return i*2
+
+#tests
+
+#should print 4
+print(double_integer(2))
+#should print 8
+print(double_integer(4))
+#should print -20
+print(double_integer(-10))
+#should print 0
+print(double_integer(0))
+#should print 200
+print(double_integer(100))
