ce7f0af9a83501bf5092b963138d8d4ce039c53e
Tony <awalker780@gmail.com>
2023-08-18 22:07:46 -0400

    solves kata

--- ten_min_walk.py
+++ ten_min_walk.py
@@ -1,7 +1,11 @@
 #codewars kata => https://www.codewars.com/kata/54da539698b8a2ad76000228/train/python
 
 def is_valid_walk(walk):
-  return len(walk) == 10
+  if (walk.count('n') == walk.count('s') and 
+        walk.count('e') == walk.count('w') and
+        len(walk) == 10):
+            return True
+  return False
 
 #tests
 
