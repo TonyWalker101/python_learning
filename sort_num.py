06318411147d1af4a1f2e9d782ba9ed083e3e684
Tony <awalker780@gmail.com>
2023-08-20 22:07:10 -0400

    solves kata

--- sort_num.py
+++ sort_num.py
@@ -1,5 +1,8 @@
 #codewars kata => https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/python
 
 def solution(nums):
-  return nums.sort()
+  if nums:
+    return nums.sort()
+  else:
+    return []
 
