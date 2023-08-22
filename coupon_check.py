092ade634be393af4e3b02477068a0dffb6514c1
Tony <awalker780@gmail.com>
2023-08-21 22:40:12 -0400

    adds test cases

--- coupon_check.py
+++ coupon_check.py
@@ -3,3 +3,7 @@
 def check_coupon(entered_code, correct_code, current_date, expiration_date):
   pass
 
+#should print True
+print(check_coupon('123','123','September 5, 2014','October 1, 2014'))
+#should print False
+print(check_coupon('123a','123','September 5, 2014','October 1, 2014'))
