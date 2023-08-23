ca79ff84f9b7066d6d2f804e94a1cc24f25cdf39
Tony <awalker780@gmail.com>
2023-08-22 23:03:02 -0400

    solves kata

--- coupon_check.py
+++ coupon_check.py
@@ -1,7 +1,11 @@
 #codewars kata => https://www.codewars.com/kata/539de388a540db7fec000642/python
 
+import datetime
+
 def check_coupon(entered_code, correct_code, current_date, expiration_date):
-  pass
+  if entered_code is correct_code:
+      return(datetime.datetime.strptime(current_date,'%B %d, %Y') <= datetime.datetime.strptime(expiration_date,'%B %d, %Y'))
+  return False
 
 #should print True
 print(check_coupon('123','123','September 5, 2014','October 1, 2014'))
