55afaf074bb3ac843882c08bb4fd57a3b870e7f5
Tony <awalker780@gmail.com>
2023-08-23 22:00:11 -0400

    adds test cases

--- sum_arrays.py
+++ sum_arrays.py
@@ -3,3 +3,15 @@
 def sum_array(a):
   pass
 
+#tests
+
+#should print 0
+print(sum_array([]))
+#should print 6
+print(sum_array([1, 2, 3]))
+#should print 6.6
+print(sum_array([1.1, 2.2, 3.3]))
+#should print 15
+print(sum_array([4, 5, 6]))
+#should print 5050
+print(sum_array(range(101)))
