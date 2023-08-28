4d17a54ca4b787cb785700d23fff29a0d4cf5d8c
Tony <awalker780@gmail.com>
2023-08-27 21:55:30 -0400

    adds test cases

--- string_incrementer.py
+++ string_incrementer.py
@@ -3,3 +3,21 @@
 def increment_string(strng):
   return strng
 
+#tests
+
+#should print "foo1"
+print(increment_string("foo"))
+#should print "foobar002"
+print(increment_string("foobar001"))
+#should print "foobar2"
+print(increment_string("foobar1"))
+#should print "foobar01"
+print(increment_string("foobar00"))
+#should print "foobar100"
+print(increment_string("foobar99"))
+#should print "foobar100"
+print(increment_string("foobar099"))
+#should print "fo99obar100"
+print(increment_string("fo99obar99"))
+#should print 1
+print(increment_string(""))
