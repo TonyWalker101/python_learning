1a2d0c69ce89770a62efc92207ed17859e7fbb76
Tony <awalker780@gmail.com>
2023-08-29 15:36:14 -0400

    adds test cases

--- remove_dupes.py
+++ remove_dupes.py
@@ -3,3 +3,15 @@
 def distinct(seq):
   pass
 
+#tests
+
+#should print [1]
+print(distinct([1]))
+#should print [1, 2]
+print(distinct([1, 2]))
+#should print [1, 2]
+print(distinct([1, 1, 2]))
+#should print [1, 2, 3, 4, 5]
+print(distinct([1, 1, 1, 2, 3, 4, 5]))
+#should print [1, 2, 3, 4, 5, 6, 7]
+print(distinct([1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 7, 7]))
