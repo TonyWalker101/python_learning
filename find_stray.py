c2f1b5de9c0d6d0799cc16fd4e87a6264fdbce83
Tony <awalker780@gmail.com>
2023-09-01 18:01:42 -0400

    initial solution

--- find_stray.py
+++ find_stray.py
@@ -1,5 +1,18 @@
 #codewars kata => https://www.codewars.com/kata/57f609022f4d534f05000024/python
 
 def stray(arr):
-  pass
+  results = arr[0]
+  for element in arr[1:]:
+    if results != element:
+      return element
+
+  return results
+#tests
+
+#should print 2
+print(stray([1, 1, 1, 1, 1, 1, 2]))
+#should print 3
+print(stray([2, 3, 2, 2, 2]))
+#should print 3
+print(stray([3, 2, 2, 2, 2]))
 
