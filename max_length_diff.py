a7bafbc10cce5a93eeb1a60ae3079f80214f1a48
Tony <awalker780@gmail.com>
2023-09-06 01:06:52 -0400

    adds test cases

--- max_length_diff.py
+++ max_length_diff.py
@@ -3,3 +3,17 @@
 def mxdiflg(a1, a2):
   return -1
 
+#tests
+
+#should print 13
+s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+print(mxdiflg(s1, s2))
+#should print 10
+s1 = ["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"]
+s2 = ["bbbaaayddqbbrrrv"]
+print(mxdiflg(s1, s2), 10)
+#should print -1
+s2 = []
+s1 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+print(mxdiflg(s1, s2), -1) 
