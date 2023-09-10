fcb832f666f65bc271e6605bd98c2f392d6bcae5
Tony <awalker780@gmail.com>
2023-09-09 20:30:53 -0400

    adds test cases

--- multiple_of_index.py
+++ multiple_of_index.py
@@ -3,3 +3,19 @@
 def multiple_of_index(arr):
   pass
 
+#tests
+
+#should print [-6, 32, 25]
+print(multiple_of_index([22, -6, 32, 82, 9, 25]))
+#should print [-1, 10]
+print(multiple_of_index([68, -1, 1, -7, 10, 10]))
+#should print [-11]
+print(multiple_of_index([11, -11]))
+#should print [-85, 72, 0, 68]
+print(multiple_of_index([-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68]))
+#should print [38, -44, -99]
+print(multiple_of_index([28,38,-44,-99,-13,-54,77,-51]))
+#should print [-49, 8, -60, 35]
+print(multiple_of_index([-1,-49,-1,67,8,-60,39,35]))
+#should print [0, 2, 6]
+print(multiple_of_index([0, 2, 3, 6, 9]))
