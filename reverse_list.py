e0b72a8caab3e660c782d7d3b7a81ada592797f8
Tony <awalker780@gmail.com>
2023-09-14 23:00:08 -0400

    initial solution

--- reverse_list.py
+++ reverse_list.py
@@ -1,12 +1,11 @@
 #codewars kata => https://www.codewars.com/kata/57a55c8b72292d057b000594/python
 
-def reverse(st):
-    # Your Code Here
-  return st
+def rev(st):
+  return " ".join((st.split).reverse)
 
 #tests
 
 #should print 'World Hello'
-print(reverse('Hello World'))
+print(rev('Hello World'))
 #should print 'There. Hi'
-print(reverse('Hi There.'))
+print(rev('Hi There.'))
