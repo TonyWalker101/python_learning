7d6b08cb162058b59d43c79c21bd7c87ac0501fe
Tony <awalker780@gmail.com>
2023-09-15 22:57:29 -0400

    solves kata

--- reverse_list.py
+++ reverse_list.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/57a55c8b72292d057b000594/python
 
 def rev(st):
-  return " ".join((st.split).reverse)
+  return " ".join(st.split()[::-1])
 
 #tests
 
