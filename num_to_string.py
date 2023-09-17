f6c643ab3ff1da0c588ffccf25a81773a010dbf3
Tony <awalker780@gmail.com>
2023-09-16 23:09:48 -0400

    adds test cases

--- num_to_string.py
+++ num_to_string.py
@@ -4,3 +4,13 @@ def number_to_string(num):
     # Return a string of the number here!
   pass
 
+#tests
+
+#should print '67'
+print(number_to_string(67))
+#should print '79585'
+print(number_to_string(79585))
+#should print '3'
+print(number_to_string(1+2))
+#should print '-1'
+print(number_to_string(1-2))
