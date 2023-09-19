67359f905f0cabada55f9ed64e0850526405629c
Tony <awalker780@gmail.com>
2023-09-18 22:32:40 -0400

    solves kata

--- num_to_string.py
+++ num_to_string.py
@@ -1,8 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/5265326f5fda8eb1160004c8/python
 
 def number_to_string(num):
-    # Return a string of the number here!
-  pass
+  return str(num)
 
 #tests
 
