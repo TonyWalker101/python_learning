21c2da09a2ae530a2f56a5c87cb783a5a3dbaa93
Tony <awalker780@gmail.com>
2023-09-19 21:42:35 -0400

    adds test cases

--- flatten_array.py
+++ flatten_array.py
@@ -3,3 +3,15 @@
 def flatten_and_sort(array):
   return []
 
+#tests
+
+#should print []
+print(flatten_and_sort([]))
+#should print []
+print(flatten_and_sort([[], []]))
+#should print [1]
+print(flatten_and_sort([[], [1]]))
+#should print [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(flatten_and_sort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
+#should print [1, 2, 3, 4, 5, 6, 100]
+print(flatten_and_sort([[1, 3, 5], [100], [2, 4, 6]]))
