e0649609df9637a1163f2094bb51de2777c8ea48
Tony <awalker780@gmail.com>
2023-09-22 19:01:09 -0400

    solves kata

--- how_many_lightsabers.py
+++ how_many_lightsabers.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/python
 
-def how_many_light_sabers_do_you_own(name):
-  return
+def how_many_light_sabers_do_you_own(name=""):
+  return 18 if name=="Zach" else 0
 
 #tests
 
