3ac915a52ed56ee1a9d084ced0df51e3719a404d
Tony <awalker780@gmail.com>
2023-09-23 21:29:09 -0400

    solves kata

--- bouncing_ball.py
+++ bouncing_ball.py
@@ -1,2 +1,10 @@
 #codewars kata => https://www.codewars.com/kata/5544c7a5cb454edb3c000047/python
 
+def bouncing_ball(h, bounce, window):
+  if not 0 < bounce < 1: return -1
+  count = 0
+  while h > window:
+    count += 1
+    h *= bounce
+    if h > window: count += 1
+  return count or -1
