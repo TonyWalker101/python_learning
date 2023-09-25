1d4b27baf70ca0c664a3004e1bd6973621075492
Tony <awalker780@gmail.com>
2023-09-24 20:56:37 -0400

    adds test cases

--- find_perfect_square.py
+++ find_perfect_square.py
@@ -3,3 +3,13 @@
 def find_next_square(sq):
   return -1
 
+#tests
+
+#should print 144
+print(find_next_square(121))
+#should print 676
+print(find_next_square(625))
+#should print 320356
+print(find_next_square(319225))
+#should print 15241630849
+print(find_next_square(15241383936))
