bf71f747347460efbad095f4dcb1122470a6e9f4
Tony <awalker780@gmail.com>
2023-09-28 22:36:52 -0400

    adds test cases

--- binary_addition.py
+++ binary_addition.py
@@ -3,3 +3,15 @@
 def add_binary(a,b):
   return
 
+#tests
+
+#should print "10"
+print(add_binary(1,1))
+#should print "1"
+print(add_binary(0,1))
+#should print "1"
+print(add_binary(1,0))
+#should print "100"
+print(add_binary(2,2))
+#should print "111111"
+print(add_binary(51,12))
