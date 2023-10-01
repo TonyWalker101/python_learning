34074b762137f8acb21b5860f89e2024caf4749b
Tony <awalker780@gmail.com>
2023-10-01 15:36:27 -0400

    initial solution

--- fake_binary.py
+++ fake_binary.py
@@ -1,5 +1,11 @@
 #codewars kata => https://www.codewars.com/kata/57eae65a4321032ce000002d/python
 
 def fake_bin(x):
-  pass
+  results = []
+  for num in x:
+    if num in ["0","1","2","3","4"]:
+      results.append("0")
+    else:
+      results.append("1")
+  return "".join(results)
 
