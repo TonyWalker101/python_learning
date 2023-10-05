44432f15ba5aef2d9834d8fe5db38ad38e419173
Tony <awalker780@gmail.com>
2023-10-04 21:38:36 -0400

    adds test cases

--- convert_to_num.py
+++ convert_to_num.py
@@ -2,3 +2,14 @@
 
 def string_to_number(s):
   pass
+
+#tests
+
+#should print 1234
+print(string_to_number("1234"))
+#should print 605
+print(string_to_number("605"))
+#should print 1405
+print(string_to_number("1405"))
+#should print -7
+print(string_to_number("-7"))
