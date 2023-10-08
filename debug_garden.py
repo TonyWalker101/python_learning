0f006695436537270cc5eec5d225a47369a84a50
Tony <awalker780@gmail.com>
2023-10-07 23:56:22 -0400

    initial commit

---
+++ debug_garden.py
@@ -0,0 +1,9 @@
+#codewars kata => https://www.codewars.com/kata/57158fb92ad763bb180004e7/python
+
+def rain_amount(mm)
+  if (rain_amount = 40):
+    return "You need to give your plant " + {rain_amount - 40} + " mm of water"
+  if else:
+    return "Your plant has had more than enough water for today!"
+  
+  
