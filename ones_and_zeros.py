e055c878389d38f0776d76d61bb2e7b82961fd12
Tony <awalker780@gmail.com>
2023-10-09 14:23:44 -0400

    adds test cases

--- ones_and_zeros.py
+++ ones_and_zeros.py
@@ -2,3 +2,15 @@
 
 def binary_array_to_number(arr):
   return
+
+#tests
+
+#should print 1
+print(binary_array_to_number([0,0,0,1]))
+#should print 2
+print(binary_array_to_number([0,0,1,0]))
+#should print 15 
+print(binary_array_to_number([1,1,1,1]))
+#should print 6
+print(binary_array_to_number([0,1,1,0]))
+    
