a49fb4e52cb8129aac6c67ea8051b765a7e55772
Tony <awalker780@gmail.com>
2023-10-14 17:12:42 -0400

    adds test cases

--- alternate_split.py
+++ alternate_split.py
@@ -2,3 +2,20 @@
 
 def encrypt(text, n):
   pass
+
+#tests
+
+#tests
+
+#should print "This is a test!"
+print(encrypt("This is a test!", 0))
+#should print "hsi  etTi sats!"
+print(encrypt("This is a test!", 1))
+#should print "s eT ashi tist!"
+print(encrypt("This is a test!", 2))
+#should print " Tah itse sits!"
+print(encrypt("This is a test!", 3))
+#should print "This is a test!"
+print(encrypt("This is a test!", 4))
+#should print "This is a test!"
+print(encrypt("This is a test!", -1))
