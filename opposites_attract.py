4d05da3fc896d4900a48cfe7e4ab8b47bed26967
Tony <awalker780@gmail.com>
2023-10-13 22:19:10 -0400

    solves kata

--- opposites_attract.py
+++ opposites_attract.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/555086d53eac039a2a000083/python
 
-def lovefunc( flower1, flower2 ):
-  pass
+def lovefunc(flower1, flower2):
+  return flower1 % 2 == 0 and flower2 % 2 != 0 or flower1 % 2 != 0 and flower2 % 2 == 0
 
 #should print True  
 print(lovefunc(1,4))
