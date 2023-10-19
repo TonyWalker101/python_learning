0b64fcca97b2c12c4aac0721af017c7bedd5fa4a
Tony <awalker780@gmail.com>
2023-10-18 23:41:37 -0400

    adds test cases

--- categorize_member.py
+++ categorize_member.py
@@ -3,3 +3,9 @@
 def open_or_senior(data):
   return
 
+#tests
+
+#should print ['Open', 'Senior', 'Open', 'Senior']
+print(open_or_senior([(45, 12),(55,21),(19, -2),(104, 20)]))
+#should print ['Open', 'Open', 'Senior', 'Open']
+print(open_or_senior([(16, 23),(73,1),(56, 20),(1, -1)]))
