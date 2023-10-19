e75b2808d2dfeb2f89281adea2598e3e89e4fd34
Tony <awalker780@gmail.com>
2023-10-18 23:44:49 -0400

    initial solution

--- categorize_member.py
+++ categorize_member.py
@@ -1,7 +1,13 @@
 #codewars kata => https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/python
 
 def open_or_senior(data):
-  return
+  results = []
+  for lst in data:
+    if lst[0] >= 55 and lst[1] > 7:
+      results.append("Senior")
+    else:
+      results.append("Open")
+  return results
 
 #tests
 
