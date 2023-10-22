a5cc4b1ed94c4946a9e5144298ba96a67e87f55a
Tony <awalker780@gmail.com>
2023-10-22 14:03:24 -0400

    adds test cases

--- total_points.py
+++ total_points.py
@@ -1,5 +1,18 @@
 #codewars kata => https://www.codewars.com/kata/5bb904724c47249b10000131/python
 
 def points(games):
-	pass
+  pass
 
+
+#tests
+
+#should print  30
+print(points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3']))
+#should print 10
+print(points(['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4']))
+#should print 0
+print(points(['0:1','0:2','0:3','0:4','1:2','1:3','1:4','2:3','2:4','3:4']))
+#should print 15
+print(points(['1:0','2:0','3:0','4:0','2:1','1:3','1:4','2:3','2:4','3:4']))
+#should print 12
+print(points(['1:0','2:0','3:0','4:4','2:2','3:3','1:4','2:3','2:4','3:4']))
