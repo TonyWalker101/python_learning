7bad07220f3fab75c003635be2cfa4e2d3ce5e4d
Tony <awalker780@gmail.com>
2023-10-24 21:10:17 -0400

    adds test cases

--- row_weights.py
+++ row_weights.py
@@ -3,3 +3,15 @@
 def row_weights(array):
   pass
 
+#tests
+
+#should print (80,0)
+print(row_weights([80]))
+#should print (100,50)
+print(row_weights([100,50]))
+#should print (120,140)
+print(row_weights([50,60,70,80]))
+#should print (62,27)
+print(row_weights([13,27,49]))
+#should print (236,92)
+print(row_weights([70,58,75,34,91]))
