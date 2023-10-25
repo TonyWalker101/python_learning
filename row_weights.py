503a0c2318b9c26e4fd24966c17b4eaf81f04985
Tony <awalker780@gmail.com>
2023-10-24 21:17:33 -0400

    solves kata

--- row_weights.py
+++ row_weights.py
@@ -1,7 +1,16 @@
 #codewars kata => https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/python
 
 def row_weights(array):
-  pass
+  team_1 = 0
+  team_2 = 0
+
+  for x in range(len(array)):
+    if x % 2 == 0:
+      team_1 += array[x]
+    else:
+      team_2 += array[x]
+  
+  return (team_1,team_2)
 
 #tests
 
