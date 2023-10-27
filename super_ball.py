57a82f87fc2c08e1abcd227ac5306a397f3b12b0
Tony <awalker780@gmail.com>
2023-10-26 22:16:18 -0400

    solves kata

--- super_ball.py
+++ super_ball.py
@@ -1,7 +1,10 @@
 #codewars kata => https://www.codewars.com/kata/53f0f358b9cb376eca001079/python
 
 class Ball(object):
-  pass
+
+  def __init__(self, name = ""):
+    self.name = name
+    self.ball_type = "super" if self.name == "super" else "regular"
 
 #tests
 
