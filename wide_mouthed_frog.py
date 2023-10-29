2de9664b71b388c5bd57af1983644ba8a681a7aa
Tony <awalker780@gmail.com>
2023-10-29 11:24:47 -0400

    solves kata

--- wide_mouthed_frog.py
+++ wide_mouthed_frog.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/python
 
 def mouth_size(animal): 
-  pass
+  return "small" if animal.lower() == "alligator" else "wide"
 
 #tests
 
