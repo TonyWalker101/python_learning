56214c2e90bf3dfb3121b2806968ee9b6d985963
Tony <awalker780@gmail.com>
2023-10-29 11:23:36 -0400

    adds test cases

--- wide_mouthed_frog.py
+++ wide_mouthed_frog.py
@@ -2,3 +2,12 @@
 
 def mouth_size(animal): 
   pass
+
+#tests
+
+#should print "wide"
+print(mouth_size("toucan"))
+#should print "wide"
+print(mouth_size("ant bear"))
+#should print "small"
+print(mouth_size("alligator"))
