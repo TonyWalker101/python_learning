0d5541cb80c3c0dc668f43c176ca30bf73961b87
Tony <awalker780@gmail.com>
2023-10-30 07:49:59 -0400

    adds test cases

--- title_case.py
+++ title_case.py
@@ -3,3 +3,13 @@
 def title_case(title, minor_words=''):
   pass
 
+#tests
+
+#should print ''
+print(title_case(''))
+#should print 'A Clash of Kings'
+print(title_case('a clash of KINGS', 'a an the of'))
+#should print 'The Wind in the Willows'
+print(title_case('THE WIND IN THE WILLOWS', 'The In'))
+#should print 'The Quick Brown Fox'
+print(title_case('the quick brown fox'))
