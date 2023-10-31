78b18b87887abbe9dba9d3ccc748a66acb1b9849
Tony <awalker780@gmail.com>
2023-10-31 08:18:43 -0400

    initial solution

--- title_case.py
+++ title_case.py
@@ -1,9 +1,20 @@
 #codewars kata => https://www.codewars.com/kata/5202ef17a402dd033c000009/python
 
 def title_case(title, minor_words=''):
-  lst_words = " ".join(title)
+  lst_words = title.split(" ")
+  exc_words = list(map(lambda x: x.lower(), minor_words.split(" ")))
+  results = ""
+  print(exc_words)
+  
+  for word in lst_words:
+    if word.lower() not in exc_words or lst_words.index(word) == 0:
+      results += word.title()
+    else:
+      results += word.lower()
+    
+    results += " "
 
-
+  return results.strip()
 #tests
 
 #should print ''
