232a8e65fe5ac91a3035241e1e27498b4de37dcc
Tony <awalker780@gmail.com>
2023-11-05 13:30:56 -0500

    solves kata

--- alternate_capitalization.py
+++ alternate_capitalization.py
@@ -1,13 +1,20 @@
 #codewars kata => https://www.codewars.com/kata/59cfc000aeb2844d16000075/python
 
 def capitalize(s):
-  results = []
+  string_1 = ""
+  string_2 = ""
 
-  results.append("".join(map(lambda x: x.upper() if s.index(x) % 2 == 0 else x,s.lower())))
+  for x in range(len(s)):
+    if x % 2 != 0:
+      string_1 += s[x].upper()
+      string_2 += s[x].lower()
 
-  results.append("".join(map(lambda x: x.upper() if s.index(x) % 2 != 0 else x,s.lower())))
+    if x % 2 == 0:
+      string_1 += s[x].lower()
+      string_2 += s[x].upper()
+  
+  return [string_2, string_1]
 
-  return results
 #tests
 
 #should print ['AbCdEf', 'aBcDeF']
