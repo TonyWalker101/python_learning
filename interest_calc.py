42905643a1eda6dbc05b311a541a8272969de26c
Tony <awalker780@gmail.com>
2023-11-06 08:15:46 -0500

    amends func

--- interest_calc.py
+++ interest_calc.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/563f037412e5ada593000114/python
 
 def calculate_years(principal, interest, tax, desired):
-  raise NotImplementedError("TODO: calculate_years")
+  pass
 
 #tests
 
