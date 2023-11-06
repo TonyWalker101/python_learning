8b898809fc8fe5cb7176853703559b72f76bff80
Tony <awalker780@gmail.com>
2023-11-06 08:13:48 -0500

    adds test cases

--- interest_calc.py
+++ interest_calc.py
@@ -3,3 +3,11 @@
 def calculate_years(principal, interest, tax, desired):
   raise NotImplementedError("TODO: calculate_years")
 
+#tests
+
+#should print 3
+print(calculate_years(1000, 0.05, 0.18, 1100))
+#should print 14
+print(calculate_years(1000,0.01625,0.18,1200))
+#should print 0
+print(calculate_years(1000,0.05,0.18,1000))
