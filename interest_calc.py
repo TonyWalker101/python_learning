01754b28b255f7693b7b7cda1e36c608bfad5663
Tony <awalker780@gmail.com>
2023-11-06 08:13:00 -0500

    initial commit

---
+++ interest_calc.py
@@ -0,0 +1,5 @@
+#codewars kata => https://www.codewars.com/kata/563f037412e5ada593000114/python
+
+def calculate_years(principal, interest, tax, desired):
+  raise NotImplementedError("TODO: calculate_years")
+
