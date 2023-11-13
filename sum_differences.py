82bff095ffbcc2ff00d96fdb713e73a887c3a028
Tony <awalker780@gmail.com>
2023-11-12 21:16:15 -0500

    adds test cases

--- sum_differences.py
+++ sum_differences.py
@@ -3,3 +3,21 @@
 def sum_of_differences(arr):
   return 0
 
+#tests
+
+#should print 9
+print(sum_of_differences([1, 2, 10]))
+#should print 2
+print(sum_of_differences([-3, -2, -1]))
+#should print 0
+print(sum_of_differences([1, 1, 1, 1, 1]))
+#should print 34
+print(sum_of_differences([-17, 17]))      
+#should print 0
+print(sum_of_differences([]))
+#should print 0
+print(sum_of_differences([0]))
+#should print 0
+print(sum_of_differences([-1]))
+#should print 0
+print(sum_of_differences([1]))
