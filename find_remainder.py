59d0ad253abc139998b04cfe511cea17a120339d
Tony <awalker780@gmail.com>
2023-11-15 23:47:28 -0500

    adds test cases

--- find_remainder.py
+++ find_remainder.py
@@ -2,3 +2,14 @@
 
 def remainder(a,b):
   pass
+
+#tests
+
+#should print 2
+print(remainder(17,5), 'Returned value should be the value left over after dividing as much as possible.')
+#should print -20
+print(remainder(-60, 340), 'Should handle negative numbers')
+#should print None
+print(remainder(1, 0), 'Divide by zero should return None')
+#should print None
+print(remainder(0, 0), 'Divide by zero should return None')
