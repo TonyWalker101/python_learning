9000804c4e10c4f14b11a4bd5a0efa15d6860f37
Tony <awalker780@gmail.com>
2023-11-23 13:48:01 -0500

    solves kata

--- array_filter.py
+++ array_filter.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/514a6336889283a3d2000001/python
 
 def get_even_numbers(arr):
-  return 
+  return list(filter(lambda x: x % 2 == 0, arr))
 
 #tests
 
