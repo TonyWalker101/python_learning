ce10e9691e1a47ba75adfb9f268be5fd22fa3bad
Tony <awalker780@gmail.com>
2023-11-27 23:49:31 -0500

    adds test cases

--- playing_bango.py
+++ playing_bango.py
@@ -3,3 +3,15 @@
 def are_you_playing_banjo(name):
     # Implement me!
     pass
+
+#tests
+
+#should print "martin does not play banjo"
+print(are_you_playing_banjo("martin"))
+#should print "Rikke plays banjo"
+print(are_you_playing_banjo("Rikke"))
+#should print "bravo does not play banjo"
+print(are_you_playing_banjo("bravo"))
+#should print "rolf plays banjo"
+print(are_you_playing_banjo("rolf"))
+    
