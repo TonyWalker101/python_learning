4c94c40009da841f4b074cadf71616fdb9f71001
Tony <awalker780@gmail.com>
2023-12-02 20:47:31 -0500

    adds test cases

--- count_positives.py
+++ count_positives.py
@@ -2,3 +2,18 @@
 
 def count_positives_sum_negatives(arr):
   return
+
+#tests
+
+#should print [10,-65]
+print(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+#should print [8,-50]
+print(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
+#should print [1,0]
+print(count_positives_sum_negatives([1]))
+#should print [0,-1]
+print(count_positives_sum_negatives([-1]))
+#should print [0,0]
+print(count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]))
+#should print []
+print(count_positives_sum_negatives([]))
