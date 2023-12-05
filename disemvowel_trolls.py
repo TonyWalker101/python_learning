206b4f44e6750f1c579b4b253d1b06046889c427
Tony <awalker780@gmail.com>
2023-12-04 21:33:11 -0500

    adds test cases

--- disemvowel_trolls.py
+++ disemvowel_trolls.py
@@ -3,3 +3,11 @@
 def disemvowel(string_):
   return string_
 
+#tests
+
+#should print "Ths wbst s fr lsrs LL!"
+print(disemvowel("This website is for losers LOL!"))
+#should print "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+print(disemvowel("No offense but,\nYour writing is among the worst I've ever read"))
+#should print "Wht r y,  cmmnst?"
+print(disemvowel("What are you, a communist?"))
