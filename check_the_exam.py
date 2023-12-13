1c4c81bd42cd99c043024af8352830ad6e885822
Tony <awalker780@gmail.com>
2023-12-12 20:49:39 -0500

    adds test cases

--- check_the_exam.py
+++ check_the_exam.py
@@ -2,3 +2,15 @@
 
 def check_exam(arr1,arr2):
   pass
+
+#tests
+
+#should print 6
+print(check_exam(["a", "a", "b", "b"], ["a", "c", "b", "d"]))
+#should print 7
+print(check_exam(["a", "a", "c", "b"], ["a", "a", "b",  ""]))
+#should print 16
+print(check_exam(["a", "a", "b", "c"], ["a", "a", "b", "c"]))
+#should print 0
+print(check_exam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))
+    
