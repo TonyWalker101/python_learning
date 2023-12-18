57cce8f4ecaefaff74f80e16b8a5d3d4c8946378
Tony <awalker780@gmail.com>
2023-12-17 20:12:47 -0500

    initial solution

--- age_predictor.py
+++ age_predictor.py
@@ -1,9 +1,16 @@
 #codewars kata => https://www.codewars.com/kata/5aff237c578a14752d0035ae/python
 
-def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
-  pass
+import math
+
+def predict_age(ages):
+  sum = 0
+
+  for num in ages:
+    sum += num * num
+
+  return math.floor((math.sqrt(sum))/2)
 
 #tests
 
 #should print 86
-print(predict_age(65,60,75,55,60,63,64,45))
+print(predict_age([65,60,75,55,60,63,64,45]))
