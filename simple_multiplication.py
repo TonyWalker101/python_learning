e616d3709cdc74e049d1a558f39ee527dc0f6f53
Tony <awalker780@gmail.com>
2023-12-20 22:36:37 -0500

    adds test cases

--- simple_multiplication.py
+++ simple_multiplication.py
@@ -3,3 +3,15 @@
 def simple_multiplication(number):
   pass
 
+#tests
+
+#should print 16
+print(simple_multiplication(2))
+#should print 9
+print(simple_multiplication(1))
+#should print 64
+print(simple_multiplication(8))
+#should print 32
+print(simple_multiplication(4))
+#should print 45
+print(simple_multiplication(5))
