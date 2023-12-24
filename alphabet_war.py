6d872703dbb4577d653909f2ddefec834e48e9b5
Tony <awalker780@gmail.com>
2023-12-23 21:54:11 -0500

    adds test cases

--- alphabet_war.py
+++ alphabet_war.py
@@ -3,3 +3,15 @@
 def alphabet_war(fight):
   pass
 
+#tests
+
+#should print "Right side wins!"
+print(alphabet_war("z"))
+#should print "Let's fight again!"
+print(alphabet_war("zdqmwpbs"))
+#should print "Left side wins!"
+print(alphabet_war("wq"))
+#should print "Right side wins!"
+print(alphabet_war("zzzzs"))
+#should print "Left side wins!"
+print(alphabet_war("wwwwww"))
