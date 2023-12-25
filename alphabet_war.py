8692bf89cc569836737f416618f25fc1d6ce7715
Tony <awalker780@gmail.com>
2023-12-24 21:16:54 -0700

    initial solution

--- alphabet_war.py
+++ alphabet_war.py
@@ -1,7 +1,21 @@
 #codewars kata => https://www.codewars.com/kata/59377c53e66267c8f6000027/python
 
 def alphabet_war(fight):
-  pass
+  left_side = {"w":4,"p":3,"b":2,"s":1,"score":0}
+  right_side = {"m":4,"q":3,"d":2,"z":1,"score":0}
+
+  for letter in fight:
+    if letter in left_side:
+      left_side["score"] += left_side[letter]
+    else:
+      right_side["score"] += right_side[letter]
+  
+  if left_side["score"] > right_side["score"]:
+    return "Left side wins!"
+  elif left_side["score"] < right_side["score"]:
+    return "Right side wins!"
+  else:
+    return "Let's fight again!"
 
 #tests
 
