1cc52deae62b0e57f5e741d0adaf9d2344923f48
Tony <awalker780@gmail.com>
2024-01-07 10:21:31 -0500

    adds test cases

--- largest_pair.py
+++ largest_pair.py
@@ -2,3 +2,14 @@
 
 def largest_pair_sum(numbers): 
   pass
+
+#tests
+
+#should print 42
+print(largest_pair_sum([10,14,2,23,19]))
+#should print 0
+print(largest_pair_sum([-100,-29,-24,-19,19]))
+#should print 10
+print(largest_pair_sum([1,2,3,4,6,-1,2]))
+#should print -18
+print(largest_pair_sum([-10, -8, -16, -18, -19]))
