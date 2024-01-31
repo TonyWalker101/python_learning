9576709c20455e0e86a75c4b0ab949c73f86679c
Tony <awalker780@gmail.com>
2024-01-30 23:04:23 -0500

    solves kata

--- set_alarm.py
+++ set_alarm.py
@@ -1,7 +1,7 @@
 #codewars kata => https://www.codewars.com/kata/568dcc3c7f12767a62000038/python
 
 def set_alarm(employed, vacation):
-  pass
+  return True if employed and not vacation else False
 
 #tests
 
