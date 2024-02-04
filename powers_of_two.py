b8a5f0072fed2868b798216d2f9690ae97279141
Tony <awalker780@gmail.com>
2024-02-03 23:19:07 -0500

    initial solution

--- powers_of_two.py
+++ powers_of_two.py
@@ -1,7 +1,12 @@
 #codewars kata => https://www.codewars.com/kata/57a083a57cb1f31db7000028/python
 
 def powers_of_two(n):
-  return []
+  answer = []
+
+  for num in range(0,n+1):
+    answer.append(2**n)
+  
+  return answer
 
 #tests
 
