954884bfe08f10fbe9693f90fe9e51a78d5de309
Tony <awalker780@gmail.com>
2024-02-05 08:50:09 -0500

    adds test cases

--- find_min_max_number.py
+++ find_min_max_number.py
@@ -5,3 +5,18 @@ def minimum(arr):
 
 def maximum(arr):
   pass
+
+#tests
+
+#should print -110
+print(minimum([-52, 56, 30, 29, -54, 0, -110]))
+#should print 0
+print(minimum([42, 54, 65, 87, 0]))
+#should print 1
+print(minimum([1, 2, 3, 4, 5, 10]))
+#should print 5
+print(maximum([5]))
+#should print 555
+print(maximum([534,43,2,1,3,4,5,5,443,443,555,555]))
+#should print 9
+print(maximum([9]))
