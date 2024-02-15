1f45ba60705170d7589e8c3f40e059f26c2cd294
Tony <awalker780@gmail.com>
2024-02-14 21:44:29 -0500

    adds test cases

--- name_shuffler.py
+++ name_shuffler.py
@@ -2,3 +2,12 @@
 
 def name_shuffler(str_):
   pass
+
+#tests
+
+#should print McClane john
+print(name_shuffler('john McClane'))
+#should print jeggins Mary
+print(name_shuffler('Mary jeggins'))
+#should print jerry tom
+print(name_shuffler('tom jerry'))
