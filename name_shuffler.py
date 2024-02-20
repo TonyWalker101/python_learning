74dc96edc857e78e0b9f3b9eb4905483d8837c64
Tony <awalker780@gmail.com>
2024-02-20 07:57:34 -0500

    solves kata

--- name_shuffler.py
+++ name_shuffler.py
@@ -1,8 +1,9 @@
 #codewars kata => https://www.codewars.com/kata/559ac78160f0be07c200005a/python
 
 def name_shuffler(str_):
-  name = " ".join(str_)
-  return name
+  name = str_.split()
+  name.reverse()
+  return " ".join(name)
 
 #tests
 
