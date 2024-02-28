52cf354d007abd453be18abe0b9fc66b0c448b4b
Tony <awalker780@gmail.com>
2024-02-28 18:19:18 -0500

    adds test cases

--- factorial.py
+++ factorial.py
@@ -3,3 +3,13 @@
 def factorial(n):
   pass
 
+#tests
+
+#should print factorial for 0 is 1
+print(factorial(0))
+#should print factorial for 0 is 1
+print(factorial(1))
+#should print factorial for 2 is 2
+print(factorial(2))
+#should print factorial for 3 is 6
+print(factorial(3))
